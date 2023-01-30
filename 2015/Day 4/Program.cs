10dd1adb7f47bd8dbf141af3730bdc34480bf9e9
Kezzryn <bkholmes@shaw.ca>
2023-01-29 21:31:39 -0600

    2015 Day 4 & 5

---
+++ 2015/Day 4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+static string GetHash(HashAlgorithm hashAlgorithm, string input)
+{
+    // Copied from https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.hashalgorithm.computehash
+
+    // Convert the input string to a byte array and compute the hash.
+    byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+    // Create a new Stringbuilder to collect the bytes and create a string.
+    var sBuilder = new StringBuilder();
+
+    // Loop through each byte of the hashed data and format each one as a hexadecimal string.
+    for (int i = 0; i < data.Length; i++)
+    {
+        sBuilder.Append(data[i].ToString("x2"));
+    }
+
+    // Return the hexadecimal string.
+    return sBuilder.ToString();
+}
+
+
+try
+{
+    const string PUZZLE_INPUT = "PuzzleInput.txt";
+
+    string secretKey = File.ReadAllText(PUZZLE_INPUT);
+
+    using MD5 md5Hash = MD5.Create();
+
+    bool isDoneP1 = false;
+    bool isDoneP2 = false;
+
+    long counter = 0;
+    long part1Answer = 0;
+    long part2Answer = 0;
+    string hash = "";
+
+    while (!(isDoneP2 && isDoneP2))
+    {
+        counter++;
+        hash = GetHash(md5Hash, $"{secretKey}{counter}");
+
+        if ( !isDoneP1 )
+        {
+            if (hash.StartsWith("00000"))
+            {
+                part1Answer = counter;
+                isDoneP1 = true;
+            }
+        }
+
+        if (!isDoneP2)
+        {
+            if (hash.StartsWith("000000"))
+            {
+                part2Answer = counter;
+                isDoneP2 = true;
+            }
+        }
+    }
+
+    Console.WriteLine($"Part1: The first five zero AdventCoin hash is at {part1Answer}.");
+    Console.WriteLine($"Part2: The first six zero AdventCoin hash is at {part2Answer}.");
+
+} 
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+
---
+++ 2015/Day 5/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Text.RegularExpressions;
+
+bool NiceStringPart1(string str)
+{
+    string[] badStrings = 
+    {
+        "ab","cd","pq","xy"
+    };
+
+    //  It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+    if (badStrings.Any(str.Contains)) return false;
+
+    //  It contains at least three vowels(aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+    int vowelCount = str.Where(c => "aeiou".ToCharArray().Contains(c)).ToArray().Count();
+    
+    //  It contains at least one letter that appears twice in a row, like xx, abcdde(dd), or aabbccdd(aa, bb, cc, or dd).
+    int doubleLettersCount = Regex.Matches(str, @"(.)\1").Count;
+
+    return (vowelCount >= 3 && doubleLettersCount >= 1);
+}
+
+bool NiceStringPart2(string str)
+{
+    //It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+    //It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi(efe), or even aaa.
+
+
+    return false;
+}
+
+try
+{
+    const string PUZZLE_INPUT = "PuzzleInput.txt";
+
+    string[] puzzleInput = File.ReadAllLines(PUZZLE_INPUT);
+
+    int nicePart1 = puzzleInput
+        .Select(NiceStringPart1)
+        .ToArray()
+        .Sum(x => x ? 1 : 0);
+
+    int nicePart2 = puzzleInput
+        .Select(NiceStringPart2)
+        .ToArray()
+        .Sum(x => x ? 1 : 0);
+
+    Console.WriteLine($"Part 1: The number of entries on the nice list are {nicePart1}.");
+    Console.WriteLine($"Part 2: The number of entries on the nice list are {nicePart2}.");
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
