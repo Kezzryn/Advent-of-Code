e1137f9854427957ea63ded5ba2bc498ae0cdf0d
Kezzryn <bkholmes@shaw.ca>
2023-02-27 15:13:02 -0600

    Basic framework done. Working on self test.

---
+++ Synacor Challenge/Decompiler.cs
@@ -0,0 +1,70 @@
+﻿namespace Synacor_Challenge
+{
+    internal static class Decompiler
+    {
+        static public readonly Dictionary<int, (string inst, int num)> instructionSet = new()
+        {
+            {  0, ("halt", 0) },
+            {  1, ("set", 2) },
+            {  2, ("push", 1) },
+            {  3, ("pop", 1) },
+            {  4, ("eq", 3) },
+            {  5, ("gt", 3) },
+            {  6, ("jmp", 1) },
+            {  7, ("jt", 2) },
+            {  8, ("jf", 2) },
+            {  9, ("1dd", 3) },
+            { 10, ("mult", 3) },
+            { 11, ("mod", 3) },
+            { 12, ("1nd", 3) },
+            { 13, ("or", 3) },
+            { 14, ("not", 2) },
+            { 15, ("rmem", 2) },
+            { 16, ("wmem", 2) },
+            { 17, ("c1ll", 1) },
+            { 18, ("ret", 0 ) },
+            { 19, ("out", 1) },
+            { 20, ("in", 1) },
+            { 21, ("noop", 0) }
+        };
+
+        static public void DumpIt(BinaryReader reader)//, StreamWriter writer)
+        {
+
+            // address 
+            // instruction 
+            // parameters (up to 3) 
+            // column formatted :P 
+
+            //0: mult 12345 12345 12345
+
+            reader.BaseStream.Seek(0, SeekOrigin.Begin);
+            while (reader.BaseStream.Position < 1000) //r.BaseStream.Length)
+            {
+                int instr = reader.ReadUInt16();
+
+                string line;
+                Console.Write($"{reader.BaseStream.Position / 2}  {instr}: {instructionSet[instr].inst} ");
+
+                switch (instr)
+                {
+                    case 19:
+                        int nextChar = reader.ReadUInt16();
+                        line = (nextChar == '\n') ? "newline" : ((char)nextChar).ToString();
+                    break;
+                default:
+                    line = instructionSet[instr].num switch
+                    {
+                        1 => $"{reader.ReadUInt16()}",
+                        2 => $"{reader.ReadUInt16()} {reader.ReadUInt16()}",
+                        3 => $"{reader.ReadUInt16()} {reader.ReadUInt16()} {reader.ReadUInt16()}",
+                        _ => ""
+                    };
+                    break;
+                };
+
+                Console.WriteLine(line);    
+            }
+        } 
+    }
+}
--- Synacor Challenge/Program.cs
+++ Synacor Challenge/Program.cs
@@ -1,2 +1,153 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Synacor_Challenge;
+
+try
+{
+    const ushort MAX_VALUE = 36768;
+    const ushort ABSOLUTE_MAX = 36776;
+
+    const string CHALLENGE_BIN = "challenge.bin";
+
+    ushort[] mainMemory = new ushort[MAX_VALUE];
+    ushort[] registers = new ushort[8];
+    Stack<ushort> stack = new();
+
+    ushort cursor = 0;
+    bool isRunning = true;
+
+    ushort GetValue(ushort address)
+    {
+        return address switch
+        {
+            <= MAX_VALUE => mainMemory[address],
+            > MAX_VALUE and <= ABSOLUTE_MAX => registers[address % MAX_VALUE],
+            _ => throw new Exception($"{address} is out of bounds")
+        };
+    }
+
+    ushort DoInstruction(ushort cursor, ushort[] param)
+    {
+        ushort instruction = mainMemory[cursor]; // will throw an error if we're out of bounds, we'll keep that for now. 
+        ushort nextInstruction = cursor;
+
+        ushort a = (param.Length >= 1) ? param[0] : (ushort)0;
+        ushort b = (param.Length >= 2) ? param[1] : (ushort)0;
+        ushort c = (param.Length >= 3) ? param[2] : (ushort)0;
+
+        switch (instruction)
+        {
+            case 0:     // halt         stop execution and terminate the program
+                isRunning = false;
+                break;
+            case 1:     // set: 1 a b       set register <a> to the value of <b>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 3);
+                break;
+            case 2:     // push: 2 a	    push <a> onto the stack
+                stack.Push(a);
+                nextInstruction = (ushort)(cursor + 2);
+                break;
+            case 3:     // pop: 3 a	        remove the top element from the stack and write it into <a>; empty stack = error
+                Console.WriteLine($"{ instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 2);
+                break;
+            case 4:     // eq: 4 a b c      set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 4);
+                break;
+            case 5:     // gt: 5 a b c      set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 4);
+                break;
+            case 6:     // jmp: 6 a 	    jump to <a>
+                nextInstruction = GetValue(a);
+                break;
+            case 7:     // jt: 7 a b	    if <a> is nonzero, jump to <b>
+                nextInstruction = (GetValue(a) != 0) ? b : (ushort)(cursor + 3);
+                break;
+            case 8:     // jf: 8 a b    	if <a> is zero, jump to <b>
+                nextInstruction = (GetValue(a) == 0) ? b : (ushort)(cursor + 3);
+                break;
+            case 9:     // add: 9 a b c     assign into <a> the sum of <b> and <c> (modulo 32768)
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 4);
+                break;
+            case 10:    // mult: 10 a b c	store into <a> the product of <b> and <c> (modulo 32768)
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 4);
+                break;
+            case 11:    // mod: 11 a b c	store into <a> the remainder of <b> divided by <c>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 4);
+                break;
+            case 12:    // and: 12 a b c	stores into <a> the bitwise and of <b> and <c>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 4);
+                break;
+            case 13:    // or: 13 a b c	    stores into <a> the bitwise or of <b> and <c>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 4);
+                break;
+            case 14:    // not: 14 a b	    stores 15-bit bitwise inverse of <b> in <a>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 3);
+                break;
+            case 15:    // rmem: 15 a b	    read memory at address <b> and write it to <a>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 3);
+                break;
+            case 16:    // wmem: 16 a b	    write the value from <b> into memory at address <a>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 3);
+                break;
+            case 17:    // call: 17 a	    write the address of the next instruction to the stack and jump to <a>
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 2);
+                break;
+            case 18:    // ret: 18	        remove the top element from the stack and jump to it; empty stack = halt
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 1);
+                break;
+            case 19:    // out: 19 a        write the character represented by ascii code <a> to the terminal
+                Console.Write((char)a);
+                nextInstruction = (ushort)(cursor + 2);
+                break;
+            case 20:    // in: 20 a	        read a character from the terminal and write its ascii code to <a>; it can be assumed that once input starts, it will continue until a newline is encountered; this means that you can safely read whole lines from the keyboard and trust that they will be fully read
+                Console.WriteLine($"{instruction} not implemented yet");
+                nextInstruction = (ushort)(cursor + 2);
+                break;
+            case 21:    // noop: 21         no operation
+                nextInstruction = (ushort)(cursor + 1);
+                break;
+            default:
+                throw new NotImplementedException($"{instruction} {a} {b} {c}");
+        }
+
+        return nextInstruction;
+    }
+
+    // LOAD
+    using BinaryReader r = new(new FileStream(CHALLENGE_BIN, FileMode.Open, FileAccess.Read));
+    while (r.BaseStream.Position < r.BaseStream.Length)
+    {
+        mainMemory[r.BaseStream.Position / 2] = r.ReadUInt16();
+    }
+
+    // RUN
+    while (isRunning)
+    {
+        ushort[] param = mainMemory[int.Min(cursor + 1, MAX_VALUE -1)..int.Min(cursor + 4, MAX_VALUE - 1)];
+        //Console.Write($"{Decompiler.instructionSet[mainMemory[cursor]].inst}, {param[0]}, {param[1]}, {param[2]} ");
+
+        cursor = DoInstruction(cursor, param); // error handle this, 'cause I bet it blows up one day. 
+        //Console.WriteLine();
+    }
+
+    //Decompiler.DumpIt(r);
+    //r.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+
