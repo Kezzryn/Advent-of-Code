af7c1a1c3e24d3103bafaa6e37a88bdfa2a1fc1a
Kezzryn <bkholmes@shaw.ca>
2023-03-05 08:18:52 -0600

    Terminal works. about 90%

--- Synacor Challenge/Program.cs
+++ Synacor Challenge/Program.cs
@@ -1,16 +1,69 @@
 ﻿using Synacor_Challenge;
+using System.Diagnostics;
+using System.Text;
+
 try
 {
-    const string CHALLENGE_BIN = "challenge.bin";
-    //const string CHALLENGE_DUMP = @"..\..\..\challenge.dump";
-   
     Synacor9000 syn9k = new();
-        
-    syn9k.Load(new(new FileStream(CHALLENGE_BIN, FileMode.Open, FileAccess.Read)));
+    bool isDone = false;
 
-    //Decompiler.DumpIt(new(new FileStream(CHALLENGE_BIN, FileMode.Open, FileAccess.Read)),new(new FileStream(CHALLENGE_DUMP, FileMode.Create, FileAccess.Write)));
+    Console.TreatControlCAsInput = false;
 
-    syn9k.Run();
+    void WriteError(string message)
+    {
+        ConsoleColor ccFC = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("*** ERROR : ");
+        Console.ForegroundColor = ccFC;
+        Console.WriteLine(message);
+    }
+
+    void DoSomething(string command)
+    {
+        string[] split = command.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        switch (split[0])
+        {
+            case "exit":
+                isDone = true;
+                Console.WriteLine("Bye!");
+                break;
+            case "load" or "save":
+                string fileName = (split.GetUpperBound(0) >= 1) ? split[1] : "challenge.bin";
+                try
+                {
+                    if (split[0] == "save") syn9k.Save(fileName);
+                    if (split[0] == "load") syn9k.Load(fileName);
+                    Console.WriteLine($"{split[0]}ed {fileName}");
+                }
+                catch (Exception e)
+                {
+                    WriteError(e.Message);
+                }
+                break;
+            case "run":
+                syn9k.Run();
+                break;
+            case "help":
+                Console.WriteLine("Yea, we need some.");
+                break;
+            default:
+                Console.WriteLine(split[0]);
+                Console.WriteLine("Command not recognized.");
+                break;
+        }
+    }
+
+    Console.Clear();
+    Console.WriteLine("Welcome to the puzzle.");
+
+    while (!isDone)
+    {
+            Console.WriteLine();
+            Console.Write("s9k> ");
+            string? input = Console.ReadLine();
+            if (input != null) DoSomething(input);
+    }
+    
 
 }
 catch (Exception e)
--- Synacor Challenge/Synacor9000.cs
+++ Synacor Challenge/Synacor9000.cs
@@ -1,4 +1,8 @@
-﻿namespace Synacor_Challenge
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Synacor_Challenge
 {
     internal partial class Synacor9000
     {
@@ -11,17 +15,63 @@
         private readonly ushort[] _registers = new ushort[8];
         private readonly Stack<ushort> _stack = new();
 
+        string? _inputBuffer = string.Empty;
+        bool _stopExecution; 
+
         public Synacor9000() {}
-        public void Load(BinaryReader reader)
+        public void Load(string loadFileName)
         {
+            using BinaryReader reader = new(new FileStream(loadFileName, FileMode.Open, FileAccess.Read));
+
             while (reader.BaseStream.Position < reader.BaseStream.Length)
             {
-                _mainMemory[reader.BaseStream.Position / 2] = reader.ReadUInt16();
+                ushort address = (ushort)(reader.BaseStream.Position / 2);
+                ushort value = reader.ReadUInt16();
+
+                if (address < INVALID_MEMORY)
+                {
+                    Mem_Write(address, value);
+                }
+                else
+                {
+                    _stack.Push(value);
+                }
             }
+
+            // need to add the capacity to load registers and the stack.
+            // registers are easy, they're just higher things.. 
+            // need to restore the state of the _instrPtr as well. 
+        }
+
+        public void Save(string saveFileName)
+        {
+            using BinaryWriter bw = new(new FileStream(saveFileName, FileMode.OpenOrCreate, FileAccess.Write));
+
+            for(ushort i = 0; i < INVALID_MEMORY; i++)
+            {
+                bw.Write(Mem_Read(i));
+            }
+            foreach (ushort stackValue in _stack.Reverse())
+            {
+                bw.Write(Mem_Read(stackValue));
+            }
+
+            // A stack can be enumerated without disturbing its contents.
+           // foreach (string number in numbers)
+            //{
+            //    Console.WriteLine(number);
+           // }
+           //NOTE This return LAST PUSHED ITEM FIRST 
+           // push 1, push 2, push 3, returns 3,2,1 
+
+            // need to add the capacity to save registers and the stack.
+            // registers are easy, they're just higher memory things. 
+            // need to restore the state of the _instrPtr as well. 
         }
         public void Run()
         {
-            while (true)
+            _stopExecution = false;
+            while (!_stopExecution)
             {
                 Dispatcher(Ptr_ReadValue());
             }
@@ -31,7 +81,7 @@
             switch (instruction)
             {
                 case 0:
-                    Environment.Exit(0);
+                    _stopExecution = true;
                     break;
                 case 1 or 15 or 16:
                     Mem_Manipulation(instruction);
@@ -60,17 +110,79 @@
         }
         private void Terminal(ushort instruction)
         {
+            const char NEWLINE = '\n';
+
             switch (instruction)
             {
-                case 19:                                    // out: 19 a    write the character represented by ascii code <a> to the terminal
+                case 19:         // out: 19 a    write the character represented by ascii code <a> to the terminal
                     Console.Write((char)Ptr_ReadValue());
                     break;
-                case 20:                                    // in: 20 a     read a character from the terminal and write its ascii code to <a>;
+                case 20:        // in: 20 a     read a character from the terminal and write its ascii code to <a>;
                     // it can be assumed that once input starts, it will continue until a newline is encountered;
                     // this means that you can safely read whole lines from the keyboard and trust that they will be fully read
-                    ushort address = Ptr_ReadRaw();
-                    ushort value = (ushort)Console.Read();
-                    Mem_Write(address, value);
+                    ConsoleKeyInfo cki;
+                    StringBuilder sb = new();
+                    if (string.IsNullOrEmpty(_inputBuffer))
+                    {
+                        do
+                        {
+                            cki = Console.ReadKey();
+                            switch (cki.Key)
+                            {
+                                case ConsoleKey.F5:
+                                    Console.WriteLine("Quicksave coming soon...");
+                                    //Save("quicksave.bin");
+                                    break;
+                                case ConsoleKey.F7:
+                                    Console.WriteLine("Quickload coming soon...");
+                                    //Load("quicksave.bin");
+                                    break;
+                                case ConsoleKey.Escape:
+                                    _stopExecution = true;
+                                    break;
+                                case ConsoleKey.Backspace:
+                                    sb = sb.Remove(sb.Length - 1, 1);
+                                    // backspace moves the cursor back, so whitespace, then \b to back the cursor up again. 
+                                    Console.Write(" \b");
+                                    break;
+                                case ConsoleKey.Enter:
+                                    Console.WriteLine();
+                                    sb.Append(NEWLINE);
+                                    break;
+                                default:
+                                    sb.Append(cki.KeyChar);
+                                    break;
+                            }
+                            // if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+                            // if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+                            // if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+                        } while (cki.Key != ConsoleKey.Enter && cki.Key != ConsoleKey.Escape);
+
+                        // don't pass back ENTER only.
+                        // capture other stuff. 
+                        _inputBuffer = string.IsNullOrEmpty(sb.ToString()) || sb.ToString()[0] == NEWLINE ? string.Empty : sb.ToString();
+                    } // end reading from the console. 
+
+                    if (_stopExecution || string.IsNullOrEmpty(_inputBuffer))
+                    {
+                        _instPtr--; // back this up to what should be the instruction, so if/when we resume we don't freak out.
+                        break;
+                    }
+                    else
+                    {
+                        switch (_inputBuffer)
+                        {
+                            default:
+                        }
+
+                        ushort value = _inputBuffer[0];
+                        ushort address = Ptr_ReadRaw();
+
+                        _inputBuffer = _inputBuffer.Length > 1 ? _inputBuffer[1..] : string.Empty;
+                       
+                        Mem_Write(address, value);
+                    }
+                    
                     break;
                 default:
                     throw new NotImplementedException();    // PEBCAK 
