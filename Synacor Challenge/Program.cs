fe1649ec678b5907552af943e6b60674ebf83ff8
Kezzryn <bkholmes@shaw.ca>
2023-03-14 23:56:43 -0500

    Orb puzzle solved. All puzzles solved.

--- Synacor Challenge/Program.cs
+++ Synacor Challenge/Program.cs
@@ -10,16 +10,12 @@ try
     Dictionary<ConsoleKey, string> quickCommands = new()
         {
             { ConsoleKey.Escape,    $"{ProgCmds.CMD_CHAR}{ProgCmds.Exit}" },
-            { ConsoleKey.F2,        $"{ProgCmds.CMD_CHAR}break addy 5482" },
-            { ConsoleKey.F3,        $"{ProgCmds.CMD_CHAR}set input use teleporter" },
-            { ConsoleKey.F4,        $"{ProgCmds.CMD_CHAR}set register 7 1" },
-            { ConsoleKey.F5,        $"{ProgCmds.CMD_CHAR}break run" },
-            { ConsoleKey.F6,        $"{ProgCmds.CMD_CHAR}trace echo" },
-            { ConsoleKey.F7,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Solve_TP}" },
-            { ConsoleKey.F8,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Load} teleporter.syn9k" },
-            { ConsoleKey.F9,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Solve_ORB}" },
-            { ConsoleKey.F10,       $"{ProgCmds.CMD_CHAR}step 5" },
-            { ConsoleKey.F11,       $"{ProgCmds.CMD_CHAR}step 50" }
+            { ConsoleKey.F2,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Load} challenge.bin" },
+            { ConsoleKey.F3,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Load}run" },
+            { ConsoleKey.F5,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Load} teleporter.syn9k" },
+            { ConsoleKey.F6,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Solve_TP}" },
+            { ConsoleKey.F7,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Load} orb.syn9k" },
+            { ConsoleKey.F8,        $"{ProgCmds.CMD_CHAR}{ProgCmds.Solve_ORB}" }
         };
     //Dictionary<string, (int Left, int Top)> curPos = new()
     //    {
@@ -72,13 +68,13 @@ try
         Console.WriteLine($"{"F1",spc} This message.");
         foreach (var cmd in ProgCmds.Command_Descriptions)
         {
-            Console.WriteLine($"{cmd.Key, spc} {cmd.Value}");
+            Console.WriteLine($"{cmd.Key,spc} {cmd.Value}");
         }
         Console.WriteLine();
         Console.WriteLine($"All text that is not preceeded by {ProgCmds.CMD_CHAR} is passed without modification to the running program.");
         Console.WriteLine();
         Console.WriteLine("Quick-keys:");
-        foreach(KeyValuePair<ConsoleKey, string> cmd in quickCommands)
+        foreach (KeyValuePair<ConsoleKey, string> cmd in quickCommands)
         {
             Console.WriteLine($"{cmd.Key} {cmd.Value}");
         }
@@ -128,67 +124,32 @@ try
                     WriteError(results);
                 break;
             case ProgCmds.Solve_TP:
-                WriteMessage($"Searching space and time.");
-                Stopwatch sw = Stopwatch.StartNew();
-
-                /*
-                 * 
-                 * 
-                  PAckAsync(int reg7)
-        {
-            // This is a procedural Ackermann implementation. 
-            // Copied verbatum from Andy Christianson at https://github.com/NiXXeD/
-            // A solution that I wouldn't have arrived at, but is obvious in hindsight. 
-
-            // r0 goes from 4 to 0.  r1 can be between 0 and 32768, so that's our max search space.
-            int[,] cache = new int[5, 32769];
-
-            //   (0, _) => AddOne(r1)
-            for (int i = 0; i <= 32768; i++)
-            {
-                cache[0, i] = (i + 1) % 32768;
-            }
-
-            // fill in each row of our solution space based on date from the previous rows.
-            for (int r0 = 1; r0 <= 4; r0++)
-            {
-                //  (_, 0) => Ack(SubOne(r0), regSeven)
-                cache[r0, 0] = cache[r0 - 1, reg7];
-
-                // This one took me a hot minute. The second part represents the state that only exists in the first few calls of the function. 
-                // After that's out of the way, r1 increases to the max of 32768 before rolling over to 0, which we handle on the previous line.
-                // but, because we're counting up and not down, we need 
-                for (int r1 = 1; (r1 < 32768 && r0 < 4) || (r1 < 2 && r0 == 4); r1++)
+                if (!isLoaded)
                 {
-                    if (cancellationToken.IsCancellationRequested) return Task.FromCanceled<int>(cancellationToken);
-                    //Ack(SubOne(r0), Ack(r0, SubOne(r1)))
-                    cache[r0, r1] = cache[r0 - 1, cache[r0, r1 - 1]];
+                    WriteMessage($"No binary loaded.");
+                    break;
                 }
-            }
+                
+                WriteMessage($"Searching space and time.");
 
-            //and echo back the input that gave us the correct result. 
-            return Task.FromResult(cache[4, 1] == TARGET_VALUE ? reg7 : -1);
-        }
-                 */
-
-                int TP_Code = PuzzleSolutions.Solve().Result;
+                Stopwatch sw = Stopwatch.StartNew();
+                int TP_Code = PuzzleSolutions.Solve_Teleporter().Result;
                 sw.Stop();
 
-
-                WriteMessage($"The teleporter solution code found in {sw.ElapsedMilliseconds / 1000} seconds. {TP_Code}");
+                WriteMessage($"The teleporter solution code found in {sw.ElapsedMilliseconds / 1000} seconds That's a lot less than a billion years! The code is: {TP_Code}.");
                 WriteMessage($"Rewiring teleporter.");
                 List<string> rewire = new()
                 {
                     $"set register 7 {TP_Code}",
-                    "set register 0 6",
+                    "set memory 5485 6",
                     "set memory 5489 21",
                     "set memory 5490 21"
                 };
-                foreach(string tp_Command in rewire)
+                foreach (string tp_Command in rewire)
                 {
                     List<string> tp_responses = new();
                     bool res = syn9k.DebugDispatcher(tp_Command, out tp_responses);
-                    foreach(string debug_response in tp_responses)
+                    foreach (string debug_response in tp_responses)
                     {
                         WriteDebuggerOutput(debug_response, res);
                     }
@@ -196,7 +157,10 @@ try
 
                 break;
             case ProgCmds.Solve_ORB:
-                WriteMessage($"The orb solution code is: {PuzzleSolutions.OrbSolver()}");
+                List<string> solution = PuzzleSolutions.Solve_Orb();
+                StringBuilder sb = new();
+                foreach (string s in solution) sb.Append(s);
+                WriteMessage($"The orb solution code is: {sb}");
                 break;
             default:
                 // pass all unknown !commands commands through. Maybe they're for the debugger?
--- Synacor Challenge/PuzzleSolutions.cs
+++ Synacor Challenge/PuzzleSolutions.cs
@@ -1,14 +1,32 @@
-﻿namespace Synacor_Challenge
+﻿using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Synacor_Challenge
 {
+    internal record OrbMazeState
+    {
+        public Point pos = new(0, 0);
+        public int Steps = 0;
+        public int Weight = 22;
+        public List<string> Path = new();
+
+        public OrbMazeState() { }
+
+        public OrbMazeState(OrbMazeState omz)
+        {
+            Path = new();
+            pos = omz.pos;
+            Steps = omz.Steps;
+            Weight = omz.Weight;
+            foreach (string s in omz.Path) { Path.Add(s); }
+        }
+    }
+
     internal static class PuzzleSolutions
     {
         const int TARGET_VALUE = 6;
 
-        //  public static int RegToInt((ushort r0, ushort r1) r) => RegToInt(r.r0, r.r1);
-        //  public static int RegToInt(ushort r0, ushort r1) => (r0 << 16) | (r1 & 0xffff);
-        //  private static (ushort reg0, ushort reg1) IntToReg(int i) => ((ushort)(i >> 16), (ushort)i);
-
-        public static async Task<int> Solve()
+        public static async Task<int> Solve_Teleporter()
         {
             const int INTERVAL = 1000;
             const int MAX = 32767;
@@ -22,13 +40,12 @@
             while (!isDone)
             {
                 Console.WriteLine($"Loading taskgroup: {regSeven}");
-                for(int i = regSeven; i < (regSeven + INTERVAL) && i < MAX; i++)
+                for (int i = regSeven; i < (regSeven + INTERVAL) && i < MAX; i++)
                 {
                     ack.Add(PAckAsync(i, cancel_token));
                 }
                 regSeven += INTERVAL;
 
-                Console.WriteLine($"Processing taskgroup.");
                 while (ack.Count > 0)
                 {
                     Task<int> finishedTask = await Task.WhenAny(ack);
@@ -85,20 +102,116 @@
             return Task.FromResult(cache[4, 1] == TARGET_VALUE ? reg7 : -1);
         }
 
-        public static int OrbSolver()
+        public static List<string> Solve_Orb()
         {
-            return -1;
+            const int TARGET_WEIGHT = 30;
+            const int ORB_Times = 100;
+            const int ORB_Plus = 101;
+            const int ORB_Minus = 102;
+            Point startPoint = new(0, 0);
+            Point endPoint = new(3, 3);
+
+            Dictionary<Point, int> orb_map = new()
+            {
+                { new(0,3), ORB_Times },{ new(1,3), 0 },        { new(2,3), ORB_Minus },    { new(3,3), 1 },
+                { new(0,2), 4 },        { new(1,2), ORB_Times },{ new(2,2), 11 },           { new(3,2), ORB_Times },
+                { new(0,1), ORB_Plus }, { new(1,1), 4 },        { new(2,1), ORB_Minus },    { new(3,1), 18 },
+                { new(0,0), 0 },        { new(1,0), ORB_Minus },{ new(2,0), 9 },            { new(3,0), ORB_Times }
+            };
+
+            static int NewWeight(int oldWeight, int orbOp, int mazeValue)
+            {
+                return orbOp switch
+                {
+                    ORB_Times => oldWeight * mazeValue,
+                    ORB_Plus => oldWeight + mazeValue,
+                    ORB_Minus => oldWeight - mazeValue,
+                    _ => throw new NotImplementedException()
+                };
+            }
+
+            static char MapToSymbol(int symbol)
+            {
+                return symbol switch
+                {
+                    ORB_Times => '*',
+                    ORB_Plus => '+',
+                    ORB_Minus => '-',
+                    _ => throw new NotImplementedException()
+                };
+            }
+
+            bool ValidStep(Point step) => step != startPoint && orb_map.ContainsKey(step); 
+
+            List<Size> steps = new()
+            {
+                new Size( 0, +1), // N
+                new Size( 0, -1), // S
+                new Size(+1,  0), // E
+                new Size(-1,  0),  // W
+            };
+            
+            OrbMazeState start = new();
+            start.Path.Add("22");
+
+            OrbMazeState solution = new()
+            {
+                Weight = 50,
+                Steps = int.MaxValue
+            };
+
+            PriorityQueue<OrbMazeState, int> queue = new();
+            queue.Enqueue(start, start.Steps);
+
+            while (queue.Count > 0)
+            {
+                OrbMazeState current = queue.Dequeue();
+
+                if (current.Weight == TARGET_WEIGHT && current.pos == endPoint && current.Steps < solution.Steps)
+                {
+                    solution = new(current);
+                    continue;   //we have a solution! 
+                }
+
+                if (current.Weight > 100 || current.Weight < 0) continue; 
+                if (current.Steps > 50 || current.Steps > solution.Steps) continue;
+
+                foreach (Size stepOne in steps)
+                {
+                    Point testStepOne = current.pos + stepOne;
+                    if (!ValidStep(testStepOne)) continue;
+                    foreach (Size stepTwo in steps)
+                    {
+                        Point testStepTwo = testStepOne + stepTwo;
+                        if (!ValidStep(testStepTwo)) continue;
+
+                        OrbMazeState nextStep = new(current);
+                        nextStep.Steps += 2;
+                        nextStep.pos = testStepTwo;
+
+                        nextStep.Weight = NewWeight(current.Weight, orb_map[testStepOne], orb_map[testStepTwo]);
+
+                        nextStep.Path.Add($" {MapToSymbol(orb_map[testStepOne])}");
+                        nextStep.Path.Add($" {orb_map[testStepTwo]}");
+
+                        queue.Enqueue(nextStep, nextStep.Steps);
+                    }
+                }
+            }
+
+            return solution.Path;
         }
 
-
-        public static void TeleporterSolver(ushort start, ushort end)
+        private static void BADTeleporterSolver(ushort start, ushort end)
         {
+            Console.WriteLine("DO NOT USE THIS.");
+            // this works for small increments.  It needs lots of refinement if it's going to be used.
             for (ushort testValue = start; testValue <= end; testValue++)
             {
                 //18 MB seems to be min stack space that allows for solving
                 Thread tp = new(AckWrapper, 18000000);
                 tp.Start(testValue);
-                tp.Join();
+                // tp.Join();
             }
 
             void AckWrapper(object? obj)
@@ -113,12 +226,12 @@
                 ushort answer = Ack(4, 1);
                 if (answer == TARGET_VALUE)
                 {
-                    Console.WriteLine($"***Match on {regSeven}. Result {answer}.");
+                    Console.WriteLine($"***Match on {regSeven}.");
                 }
                 else
                 {
-                    Console.WriteLine($"No match on {regSeven}. Result {answer}.");
-                }    
+                    // Console.WriteLine($"No match on {regSeven}. Result {answer}.");
+                }
 
                 ushort Ack(ushort r0, ushort r1)
                 {
@@ -142,6 +255,5 @@
             }
 
         }
-
     }
 }
