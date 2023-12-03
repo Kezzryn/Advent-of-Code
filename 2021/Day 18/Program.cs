e2f17c8e245c9396649f6bcf3d82c89b0f103ef1
Kezzryn <bkholmes@shaw.ca>
2023-12-02 19:50:12 -0600

    Because I could.

--- 2021/Day 18/Program.cs
+++ 2021/Day 18/Program.cs
@@ -8,16 +8,12 @@ try
     Node summedNode = puzzleInput.Select(x => new Node(x)).Aggregate((x,y) => Node.Add(x,y));
     int part1Answer = summedNode.FindMagnitude();
 
-    List<Node> part2Nodes = new();
-    foreach ((string l, string r) in from left in puzzleInput
-                                     from right in puzzleInput
-                                     where !left.Equals(right)
-                                     select (left, right))
-    {
-        part2Nodes.Add(Node.Add(l,r));
-    }
-
-    int part2Answer = part2Nodes.Select(x => x.FindMagnitude()).Max();
+    //Turns out you CAN do this...
+    //Still doesn't mean you SHOULD. 
+    int part2Answer = (from left in puzzleInput
+            from right in puzzleInput
+            where !left.Equals(right)
+            select (left, right)).Select(x => Node.Add(x.left, x.right).FindMagnitude()).Max();
 
     Console.WriteLine($"Part 1: The magnitude of the sum of the list is {part1Answer}.");
     Console.WriteLine($"Part 2: The largest magintude of any pair of numbers is {part2Answer}.");
