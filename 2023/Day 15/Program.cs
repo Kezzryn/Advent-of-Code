3634307ed05948ebb33ff760c7e0bcac8698faeb
Kezzryn <bkholmes@shaw.ca>
2023-12-15 01:13:18 -0600

    2023 Day 15, removed potenial prefix label bug.

--- 2023/Day 15/Program.cs
+++ 2023/Day 15/Program.cs
@@ -7,7 +7,7 @@
 
     int part1Answer = puzzleInput.Sum(HASH);
 
-    List<List<string>> boxes = new();
+    List<List<(string Label, int LensPower)>> boxes = new();
     for (int i = 0; i < 256; i++) boxes.Add(new());
     
     const char OP_MINUS = '-';
@@ -17,16 +17,17 @@
         char opCode = input.Contains(OP_EQUAL) ? OP_EQUAL : OP_MINUS;
 
         string label = input[..input.IndexOfAny("-=".ToCharArray())];
+        int lensPower = opCode == OP_EQUAL ? (input[^1] - '0') : -1;
         int hashIndex = HASH(label);
 
-        int index = boxes[hashIndex].FindIndex(x => x.StartsWith(label));
+        int index = boxes[hashIndex].FindIndex(x => x.Label == label);
 
         if (opCode == OP_MINUS && index != -1) boxes[hashIndex].RemoveAt(index);
-        if (opCode == OP_EQUAL && index == -1) boxes[hashIndex].Add(input);
-        if (opCode == OP_EQUAL && index != -1) boxes[hashIndex][index] = input;
+        if (opCode == OP_EQUAL && index == -1) boxes[hashIndex].Add((label, lensPower));
+        if (opCode == OP_EQUAL && index != -1) boxes[hashIndex][index] = (label, lensPower);
     }
 
-    int part2Answer = boxes.Select((box, boxIdx) => box.Select((lens, lensIdx) => (1 + boxIdx) * (1 + lensIdx) * (lens[^1] - '0')).Sum()).Sum();
+    int part2Answer = boxes.Select((box, boxIdx) => box.Select((lens, lensIdx) => (1 + boxIdx) * (1 + lensIdx) * lens.LensPower).Sum()).Sum();
 
     Console.WriteLine($"Part 1: The hash of each line gives a sum of {part1Answer}.");
     Console.WriteLine($"Part 2: The focusing power of the lens array is {part2Answer}.");
