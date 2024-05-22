d1daf850ecdc3e57cb2d71f870e4c0bc48cce108
Kezzryn <bkholmes@shaw.ca>
2024-05-22 07:31:24 -0500

    Cleaned up extra variables and logic.

--- 2015/Day 04/Program.cs
+++ 2015/Day 04/Program.cs
@@ -1,69 +1,32 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-static string GetHash(HashAlgorithm hashAlgorithm, string input)
-{
-    // Copied from https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.hashalgorithm.computehash
-
-    // Convert the input string to a byte array and compute the hash.
-    byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-    // Create a new Stringbuilder to collect the bytes and create a string.
-    var sBuilder = new StringBuilder();
-
-    // Loop through each byte of the hashed data and format each one as a hexadecimal string.
-    for (int i = 0; i < data.Length; i++)
-    {
-        sBuilder.Append(data[i].ToString("x2"));
-    }
-
-    // Return the hexadecimal string.
-    return sBuilder.ToString();
-}
-
 try
 {
     const string PUZZLE_INPUT = "PuzzleInput.txt";
-
     string secretKey = File.ReadAllText(PUZZLE_INPUT);
+    int counter = 0;
 
     using MD5 md5Hash = MD5.Create();
 
-    bool isDoneP1 = false;
-    bool isDoneP2 = false;
+    int part1Answer = -1;
+    int part2Answer = -1;
+    byte[] hashData = [];
 
-    long counter = 0;
-    long part1Answer = 0;
-    long part2Answer = 0;
-    string hash = "";
-
-    while (!(isDoneP1 && isDoneP2))
+    while (part1Answer == -1 || part2Answer == -1)
     {
         counter++;
-        hash = GetHash(md5Hash, $"{secretKey}{counter}");
+        hashData = MD5.HashData(Encoding.UTF8.GetBytes($"{secretKey}{counter}"));
 
-        if ( !isDoneP1 )
+        if (hashData[0] == 0 && hashData[1] == 0 && hashData[2] < 10)
         {
-            if (hash.StartsWith("00000"))
-            {
-                part1Answer = counter;
-                isDoneP1 = true;
-            }
-        }
-
-        if (!isDoneP2)
-        {
-            if (hash.StartsWith("000000"))
-            {
-                part2Answer = counter;
-                isDoneP2 = true;
-            }
+            if (part1Answer == -1) part1Answer = counter;
+            if (part2Answer == -1 && hashData[2] == 0) part2Answer = counter;
         }
     }
 
-    Console.WriteLine($"Part1: The first five zero AdventCoin hash is at {part1Answer}.");
-    Console.WriteLine($"Part2: The first six zero AdventCoin hash is at {part2Answer}.");
-
+    Console.WriteLine($"Part 1: The first five zero AdventCoin hash is at {part1Answer}.");
+    Console.WriteLine($"Part 2: The first six zero AdventCoin hash is at {part2Answer}.");
 } 
 catch (Exception e)
 {
