514aa400807380c4425a668c0143e3a200ccb29d
Kezzryn <bkholmes@shaw.ca>
2024-05-24 11:55:30 -0500

    2015 Day 11 converted to lib and cleaned up.

--- 2015/Day 11/Program.cs
+++ 2015/Day 11/Program.cs
@@ -1,57 +1,32 @@
-﻿using AoC_2022_Day_25;
+﻿using BKH.Base10Converter;
 
-string NextPassword(string currentPassword)
+static string NextPassword(string currentPassword)
 {
-    const string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    Base10Converter converter = new();
-    converter.AddDictionary("ElfPassword", new TranslationKey("abcdefghjkmnpqrstuvwxyz", 0));
-
-    // Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
-    // Removed i, l, o from the translation key, as per rule 2. 
-    // Our puzzle input does not contain any of these characters.
-    // If it did, we'd have to rethink this setup.
+    TranslationMap elfDictionary = new("abcdefghjkmnpqrstuvwxyz");
+    // Removed i, l, o from the translation map, as per Rule 2: Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
 
     string newPW = "";
-    long pwTest = converter.ConvertToBase10(currentPassword, "ElfPassword");
+    long pwTest = Base10Converter.ToBase10(currentPassword, elfDictionary);
 
     bool isDone = false;
     while (!isDone)
     {
         pwTest++;
-        newPW = converter.ConvertFromBase10(pwTest, "ElfPassword");
+        newPW = Base10Converter.FromBase10(pwTest, elfDictionary);
 
         //  Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to xyz. 
         bool ruleOne = false;
-        for (int i = 0; i < alphabet.Length - 2; i++)
+        for (int i = 0; i < newPW.Length - 2; i++)
         {
-            if (newPW.Contains(alphabet[i..(i + 3)]))
-            {
-                ruleOne = true;
-                break;
-            }
+            if (newPW[i] + 1 == newPW[i + 1] && newPW[i] + 2 == newPW[i + 2]) ruleOne = true;
         }
         if (!ruleOne) continue;
 
         //  Passwords must contain at least two different,
         //  non - overlapping pairs of letters, like aa, bb, or zz.
-        bool ruleThree = false;
-        foreach (char a in alphabet)
-        {
-            int indexA = newPW.IndexOf($"{a}{a}");
-            if (indexA != -1)
-            {
-                foreach (char b in alphabet.Except(new[] { a }))
-                {
-                    int indexB = newPW.IndexOf($"{b}{b}", indexA);
-                    if (indexB != -1)
-                    {
-                        ruleThree = true;
-                        break;
-                    }
-                }
-                if (ruleThree) break;
-            }
-        }
+        bool ruleThree = (from a in newPW.Distinct()
+                          from b in newPW.Distinct().Except([a])
+                          select (a, b)).Any(x => newPW.Contains($"{x.a}{x.a}") && newPW.Contains($"{x.b}{x.b}"));
 
         isDone = ruleOne && ruleThree;
     }
@@ -65,11 +40,11 @@ try
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string puzzleInput = File.ReadAllText(PUZZLE_INPUT);
 
-    string newPasswordPart1 = NextPassword(puzzleInput);
-    string newPasswordPart2 = NextPassword(newPasswordPart1);
+    string part1Answer = NextPassword(puzzleInput);
+    string part2Answer = NextPassword(part1Answer);
 
-    Console.WriteLine($"Part 1: Santa's new password is {newPasswordPart1}.");
-    Console.WriteLine($"Part 2: Santa's next password is {newPasswordPart2}.");
+    Console.WriteLine($"Part 1: Santa's new password is {part1Answer}.");
+    Console.WriteLine($"Part 2: Santa's next password is {part2Answer}.");
 }
 catch (Exception e)
 {
