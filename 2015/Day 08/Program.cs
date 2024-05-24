0e4beb1bc1818273ff3f8f413562e237ef662f38
Kezzryn <bkholmes@shaw.ca>
2024-05-24 08:08:13 -0500

    2015 Day 08 cleaned up.

--- 2015/Day 08/Program.cs
+++ 2015/Day 08/Program.cs
@@ -5,23 +5,11 @@ try
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string[] puzzleInput = File.ReadAllLines(PUZZLE_INPUT);
     
-    int raw = 0;
-    int part1 = 0;
-    int part2 = 0;
+    int part1Answer = puzzleInput.Sum(x => x.Length - Regex.Unescape(x[1..^1]).Length);
+    int part2Answer = puzzleInput.Sum(x => $"\"{Regex.Escape(x).Replace("\"", "\\\"")}\"".Length - x.Length);
 
-
-    foreach (string input in puzzleInput)
-    {
-        string p1 = Regex.Unescape(input[1..^1]); // strip the outer quotes
-        string p2 = $"\"{Regex.Escape(input).Replace("\"","\\\"")}\""; //we need to escape the outer quotes, and add new ones. The Regex does the rest. 
-
-        raw += input.Length;
-        part1 += p1.Length;
-        part2 += p2.Length;
-    }
-
-    Console.WriteLine($"Part 1: The answer is {raw - part1}");
-    Console.WriteLine($"Part 2: The answer is {part2 - raw}");
+    Console.WriteLine($"Part 1: The difference of the raw size and the un-escaped size is {part1Answer}.");
+    Console.WriteLine($"Part 2: The difference of the escaped size and the raw size is {part2Answer}.");
 }
 catch (Exception e)
 {
