73f8cb58f306ec22011ccd813cf14425f9a8bb71
Kezzryn <bkholmes@shaw.ca>
2024-05-24 08:11:14 -0500

    More 2015 Day 08 tweaks.

--- 2015/Day 08/Program.cs
+++ 2015/Day 08/Program.cs
@@ -9,7 +9,7 @@ try
     int part2Answer = puzzleInput.Sum(x => $"\"{Regex.Escape(x).Replace("\"", "\\\"")}\"".Length - x.Length);
 
     Console.WriteLine($"Part 1: The difference of the raw size and the un-escaped size is {part1Answer}.");
-    Console.WriteLine($"Part 2: The difference of the escaped size and the raw size is {part2Answer}.");
+    Console.WriteLine($"Part 2: The difference between the escaped size and the raw size is {part2Answer}.");
 }
 catch (Exception e)
 {
