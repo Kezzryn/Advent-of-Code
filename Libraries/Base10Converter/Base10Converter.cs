50fbdbf507fc72812e64f021c97497aa2275f9d1
Kezzryn <bkholmes@shaw.ca>
2024-05-24 11:52:33 -0500

    Library Base10Converter initial commit.

---
+++ Libraries/Base10Converter/Base10Converter.cs
@@ -0,0 +1,47 @@
+﻿namespace BKH.Base10Converter
+{
+    public static class Base10Converter
+    {
+        /// <summary>
+        /// Convert to base 10.
+        /// </summary>
+        /// <param name="inputValue">Value to convert</param>
+        /// <param name="currMap">Translation map</param>
+        /// <returns>Converted value.</returns>
+        public static long ToBase10(string inputValue, TranslationMap currMap)
+        {
+            long returnValue = 0;
+            long power = 1;
+
+            foreach (char c in inputValue.Reverse())
+            {
+                returnValue += power * currMap.GetValue(c);
+                power *= currMap.Base;
+            }
+
+            return returnValue;
+        }
+        /// <summary>
+        /// Convert from a base 10 value.
+        /// </summary>
+        /// <param name="inputValue">Value to convert</param>
+        /// <param name="currMap">Translation map</param>
+        /// <returns>Converted value</returns>
+        public static string FromBase10(long inputValue, TranslationMap currMap)
+        {
+            string returnValue = "";
+
+            do
+            {
+                int digit = (int)(inputValue % currMap.Base);
+                returnValue = $"{currMap.GetChar(digit)}{returnValue}";
+
+                inputValue /= currMap.Base;
+                if (currMap.ZeroOffset > 0) inputValue += (digit - currMap.ZeroOffset > 0) ? 1 : 0; //carry check. 
+
+            } while (inputValue > 0);
+
+            return returnValue;
+        }
+    }
+}
---
+++ Libraries/Base10Converter/TranslationMap.cs
@@ -0,0 +1,51 @@
+﻿namespace BKH.Base10Converter
+{
+    public class TranslationMap(string symbols, int offset = 0)
+    {
+        /// <summary>
+        /// Default Bin, Oct, Hex and Snafu maps.
+        /// </summary>
+        public static readonly Dictionary<string, TranslationMap> Maps = new()
+        {
+            {"BIN", new TranslationMap("01")},
+            {"OCT", new TranslationMap("01234567")},
+            {"HEX", new TranslationMap("0123456789ABCDEF")},
+            {"SNAFU", new TranslationMap("=-012", 2) }   //AoC 2022 Day 25.
+        };
+
+        //symbol chart, lowest symbol to highest. 
+        private readonly string _symbolList = symbols;
+
+        //symbol chart, rearranged to move a non-zero _zeroOffset around for easier manipulation. 
+        private readonly string _symbolsOffset = symbols[offset..] + symbols[..offset];
+
+        //used for SNAFU type number sets where digits can represent negative values in their column.
+        //Marks the zero bound index of the 0 character in _symbolList
+        private readonly int _zeroOffset = offset;
+
+        /// <summary>
+        /// Given a value, return the translated representation of that character.
+        /// </summary>
+        /// <param name="val">Value to convert</param>
+        /// <returns>Symbol translation of the value.</returns>
+        public char GetChar(int val) => _symbolsOffset[val];
+
+        /// <summary>
+        /// Given a character, returns the base 10 representation of that character.
+        /// </summary>
+        /// <param name="chr">Character to translate.</param>
+        /// <returns>Base 10 representation of a character.</returns>
+        public int GetValue(char chr) => _symbolList.IndexOf(chr) - _zeroOffset;
+
+        /// <summary>
+        /// The index of the 0 item in the translation map, usually the first item.
+        /// Used for translations where a symbol can represent a negative number.
+        /// </summary>
+        public int ZeroOffset => _zeroOffset;
+
+        /// <summary>
+        /// The base of the map.
+        /// </summary>
+        public int Base => _symbolList.Length;
+    }
+}
