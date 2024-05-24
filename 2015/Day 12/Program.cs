c90884ebea17d5936966835f0dad2bc381859bbd
Kezzryn <bkholmes@shaw.ca>
2024-05-24 12:13:54 -0500

    2015 Day 12 cleaned up.

--- 2015/Day 12/Program.cs
+++ 2015/Day 12/Program.cs
@@ -1,51 +1,31 @@
 ﻿using Newtonsoft.Json.Linq;
 
-long JDocSum (JToken jToken, bool ignoreRed = false)
+static long JDocSum (JToken jToken, bool ignoreRed = false)
 {
     // Recursivly iterate through the JSON document tree, counting all the values.
     // For Part 2, we ignore any objects (and their children) with "red" in one of their values.
 
-    long returnValue = 0;
-    switch (jToken.Type)
+    return (jToken.Type) switch
     {
-        case JTokenType.Object:
-            if (ignoreRed && jToken.Values().Contains("red")) return 0;
-            
-            foreach (JToken child in jToken.Values())
-            {
-                returnValue += JDocSum(child, ignoreRed);
-            }
-            break;
-        case JTokenType.Array:
-            foreach (JToken child in jToken.Children())
-            {
-                returnValue += JDocSum(child, ignoreRed);
-            }
-            break;
-        case JTokenType.Integer:
-            return jToken.Value<long>();
-        case JTokenType.String:
-            // do nothing, no numbers to count. 
-            break;
-        default:
-            Console.WriteLine($"JT = {jToken.Type}");
-            break;
-    }
-
-    return returnValue;
+        JTokenType.Object => (ignoreRed && jToken.Values().Contains("red")) ? 0 : jToken.Values().Sum(x => JDocSum(x, ignoreRed)),
+        JTokenType.Array => jToken.Children().Sum(x => JDocSum(x, ignoreRed)),
+        JTokenType.Integer => jToken.Value<long>(),
+        JTokenType.String => 0,
+        _ => 0
+    };
 }
 
 try
 {
+    const bool IGNORE_RED = true;
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string puzzleInput = File.ReadAllText(PUZZLE_INPUT);
-    const bool IGNORE_RED = true;
 
-    long answerPart1 = JDocSum(JObject.Parse(puzzleInput));
-    long answerPart2 = JDocSum(JObject.Parse(puzzleInput), IGNORE_RED);
+    long part1Answer = JDocSum(JObject.Parse(puzzleInput));
+    long part2Answer = JDocSum(JObject.Parse(puzzleInput), IGNORE_RED);
 
-    Console.WriteLine($"Part 1: The sum of all numbers is {answerPart1}");
-    Console.WriteLine($"Part 2: The sum of all numbers, excluding red, is {answerPart2}");
+    Console.WriteLine($"Part 1: The sum of all numbers is {part1Answer}");
+    Console.WriteLine($"Part 2: The sum of all numbers, excluding red, is {part2Answer}");
 
 }
 catch (Exception e)
