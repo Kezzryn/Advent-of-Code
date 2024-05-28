cdb03bdaf31d4d1ca53b658f779bd4a68c1cf0e4
Kezzryn <bkholmes@shaw.ca>
2024-05-28 16:28:03 -0500

    2015 Day 07 refactor.

---
+++ 2015/Day 07/Node.cs
@@ -0,0 +1,62 @@
+﻿namespace AoC_2015_Day_07
+{
+	internal record Node
+	{
+		public string SourceA;
+		public string SourceB;
+		public Ops Op;
+		public ushort? Value;
+
+		public Node(string input)
+		{
+			string[] temp = input.Split(' ');
+
+            SourceA = "";
+            SourceB = "";
+
+            // Four patterns. 
+            //12345 -> aa
+            //aa -> bb
+            //NOT aa -> bb
+            //aa [AND|OR|LSHIFT|RSHIFT] bb -> cc
+            if (ushort.TryParse(input, out ushort value))
+			{
+				Value = value;
+				Op = Ops.Wire;
+            } 
+            else if (temp.Length == 1)
+            {
+                SourceA = temp[0];
+                Op = Ops.Wire;
+            }
+			else if ( temp[0] == "NOT")
+			{
+				SourceA = temp[1];
+                Op = Ops.Not;
+            } 
+			else
+			{
+                SourceA = temp[0];
+                SourceB = temp[2];
+                Op = temp[1] switch
+                {
+                    "OR" => Ops.Or,
+                    "AND" => Ops.And,
+                    "LSHIFT" => Ops.LShift,
+                    "RSHIFT" => Ops.RShift,
+                    _ => throw new Exception($"Unknown op {temp[1]}")
+                };
+            }
+		}
+
+        public enum Ops
+        {
+            And,
+            Or,
+            LShift,
+            RShift,
+            Not,
+            Wire
+        }
+    }
+}
--- 2015/Day 07/Program.cs
+++ 2015/Day 07/Program.cs
@@ -1,95 +1,61 @@
-﻿using AoC_2015_Day_7;
+﻿using AoC_2015_Day_07;
 
 try
 {
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string[] puzzleInput = File.ReadAllLines(PUZZLE_INPUT);
 
-    Dictionary<string, Node> nodes = new();
+    Dictionary<string, Node> nodes = [];
 
+    //Load the nodes. 
     foreach (string line in puzzleInput)
     {
-        string[] temp = line.Split(' ');
-        switch (temp.Length)
-        {
-            case 3:
-                // this is a wire. 
-                // All wires are [NUM] -> [wire] other than the lx -> a entry. 
-                if (UInt16.TryParse(temp[0], out UInt16 value))
-                {
-                    nodes.Add(temp[2], new Node(value));
-                }
-                else
-                {
-                    nodes.Add(temp[2], new Node(temp[0], "", Ops.Wire));
-                }
-                break;
-            case 4:
-                // this is a NOT
-                nodes.Add(temp[3], new Node(temp[1], "", Ops.Not));
-                break;
-            case 5:
-                // this is everything else. 
-                nodes.Add(temp[4], new Node(temp[0], temp[2], temp[1] switch
-                {
-                    "OR" => Ops.Or,
-                    "AND" => Ops.And,
-                    "LSHIFT" => Ops.LShift,
-                    "RSHIFT" => Ops.RShift,
-                    _ => throw new Exception($"Unknown op {temp[1]}")
-                }));
-                break;
-            default:
-                throw new Exception($"Unknown split length. {temp.Length}");
-        }
-
+        string[] temp = line.Split("->",StringSplitOptions.TrimEntries);
+        nodes.Add(temp.Last(), new Node(temp.First()));
     }
 
-    UInt16 GetValue(string nodeName)
+    ushort GetValue(string nodeName)
     {
         if (!nodes.TryGetValue(nodeName, out Node? node))
         { 
             // this captures where we have a value in one of the "source" fields. 
-            if (UInt16.TryParse(nodeName, out UInt16 value)) return value;
+            if (ushort.TryParse(nodeName, out ushort value)) return value;
 
             Console.WriteLine($"Unable to load node {nodeName}");
             return 0;
         }
 
-        if (node.Value is not null) return (UInt16)node.Value;
-
-        UInt16 returnValue = node.Op switch
+        //cache the operation results, or forever circle the tree. 
+        node.Value ??= node.Op switch
         {
             // Nearly all other wires are caught in the null test above.
             // This catches any wires that hook to other wires, specifically the 'a' wire. 
-            Ops.Wire => GetValue(node.SourceA), 
-            Ops.Not => (UInt16)~GetValue(node.SourceA),
-            Ops.Or => (UInt16)(GetValue(node.SourceA) | GetValue(node.SourceB)),
-            Ops.And => (UInt16)(GetValue(node.SourceA) & GetValue(node.SourceB)),
-            Ops.RShift => (UInt16)(GetValue(node.SourceA) >> int.Parse(node.SourceB)),
-            Ops.LShift => (UInt16)(GetValue(node.SourceA) << int.Parse(node.SourceB)),
-            _ =>  throw new Exception($"Unknown op code for {nodeName} - {node.Op}")
+            Node.Ops.Wire => GetValue(node.SourceA),
+            Node.Ops.Not => (ushort)~GetValue(node.SourceA),
+            Node.Ops.Or => (ushort)(GetValue(node.SourceA) | GetValue(node.SourceB)),
+            Node.Ops.And => (ushort)(GetValue(node.SourceA) & GetValue(node.SourceB)),
+            Node.Ops.RShift => (ushort)(GetValue(node.SourceA) >> ushort.Parse(node.SourceB)),
+            Node.Ops.LShift => (ushort)(GetValue(node.SourceA) << ushort.Parse(node.SourceB)),
+            _ => throw new Exception($"Unknown op code for {nodeName} - {node.Op}")
         };
 
-        //cache the operation results, or forever circle the tree. 
-        node.Value = returnValue;
-
-        return returnValue;
+        return (ushort)node.Value;
     }
 
-    UInt16 part1 = GetValue("a");
-    Console.WriteLine($"Part 1: The signal on wire 'a' is {part1}");
+    ushort part1Answer = GetValue("a");
+    Console.WriteLine($"Part 1: The signal on wire 'a' is {part1Answer}");
 
     // Reset for part 2 
-    nodes["b"].Value = part1;
+    nodes["b"].Value = part1Answer;
     nodes["a"].Value = null;
+    
     foreach (Node node in nodes.Values)
     {
-        if (node.Op != Ops.Wire) node.Value = null;
+        if (node.Op != Node.Ops.Wire) node.Value = null;
     }
 
-    UInt16 part2 = GetValue("a");
-    Console.WriteLine($"Part 2: The signal on wire 'a' is {part2}");
+    ushort part2Answer = GetValue("a");
+    Console.WriteLine($"Part 2: When overriding the signal on wire 'b' with {part1Answer}, 'a' returns {part2Answer}.");
 }
 catch (Exception e)
 {
