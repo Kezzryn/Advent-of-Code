3439d31ac9e5b74569f30536289254fa4b34b275
Kezzryn <bkholmes@shaw.ca>
2024-05-30 20:30:52 -0500

    2015 .net9, cleanup, and bits of refactor.

--- 2015/Day 19/Program.cs
+++ 2015/Day 19/Program.cs
@@ -1,66 +1,66 @@
 ﻿try
 {
-    const string PUZZLE_INPUT = "PuzzleInput.txt";
     const string CRLF = "\r\n";
     const string INSTRUCTION_SPLIT = " => ";
 
-    const int SOURCE = 0;
-    const int TARGET = 1;
-
+    const string PUZZLE_INPUT = "PuzzleInput.txt";
     string puzzleInput = File.ReadAllText(PUZZLE_INPUT);
 
-    string startingMolecule = puzzleInput.Split(CRLF + CRLF)[1];
-    List<string[]> puzzleTransformations = puzzleInput.Split(CRLF + CRLF)[0].Split(CRLF)
-        .Select(x => x.Split(INSTRUCTION_SPLIT)).ToList();
+    List<(string source, string target)> puzzleTransformations =
+        puzzleInput.Split(CRLF + CRLF)
+                        .First()
+                        .Split(CRLF)
+                            .Select(x => x.Split(INSTRUCTION_SPLIT))
+                                .Select(x => (x.First(), x.Last()))
+                            .ToList();
 
-    List<string> answersPart1 = new();
+    string startingMolecule = puzzleInput.Split(CRLF + CRLF).Last();
 
-    foreach(string[] transform in puzzleTransformations)
+    HashSet<string> part1Answers = [];
+
+    foreach((string source, string target) in puzzleTransformations)
     {
-        int cutIndex = startingMolecule.IndexOf(transform[SOURCE]);
+        int cutIndex = startingMolecule.IndexOf(source);
         while (cutIndex != -1) {
             int startIndex = cutIndex;
 
-            answersPart1.Add(startingMolecule
-                .Remove(startIndex, transform[SOURCE].Length)
-                .Insert(startIndex, transform[TARGET])
+            part1Answers.Add(startingMolecule
+                .Remove(startIndex, source.Length)
+                .Insert(startIndex, target)
             );
 
-            cutIndex = startingMolecule.IndexOf(transform[SOURCE], startIndex + 1);
+            cutIndex = startingMolecule.IndexOf(source, startIndex + 1);
         }
     }
 
-    Console.WriteLine($"Part 1: There are {answersPart1.Distinct().Count()} combos.");
+    Console.WriteLine($"Part 1: There are {part1Answers.Count} combos.");
 
     // Part 2. 
     // We need to reverse the process, so let's try for a right back search,
     // looking for the biggest matches first and reducing our item back to what it was. 
     
-    string reduction = startingMolecule;
-    int cutCounter = 0;
+    int part2Answer = 0;
     bool isDone = false; //failsafe in case we get to a place where we have no matches, but don't have a way forward.
-    while (reduction.Length > 1 || !isDone)
+    while (startingMolecule.Length > 1 || !isDone)
     {
         isDone = true;
-        foreach (string[] transform in puzzleTransformations.OrderByDescending(x => x[TARGET].Length))
+        foreach ((string source, string target) in puzzleTransformations.OrderByDescending(x => x.target.Length))
         {
-            int cutIndex = reduction.LastIndexOf(transform[TARGET]);
+            int cutIndex = startingMolecule.LastIndexOf(target);
             while (cutIndex != -1)
             {
-                int startIndex = cutIndex;
-
-                reduction = reduction
-                    .Remove(startIndex, transform[TARGET].Length)
-                    .Insert(startIndex, transform[SOURCE]);
+                startingMolecule = startingMolecule
+                    .Remove(cutIndex, target.Length)
+                    .Insert(cutIndex, source);
 
                 isDone = false;
-                cutCounter++;
-                cutIndex = reduction.LastIndexOf(transform[TARGET], int.Max(startIndex - 1,0));
+                part2Answer++;
+                cutIndex = startingMolecule.LastIndexOf(target, int.Max(cutIndex - 1,0));
             }
         }
     }
 
-    Console.WriteLine($"Part 2: We need to make {cutCounter} replacements to get to the medicine molecule.");
+    Console.WriteLine($"Part 2: We need to make {part2Answer} replacements to get to the medicine molecule.");
 
     // ... or it turns out after looking at Reddit, that we can count certain symbols and do math and THAT works because this is
     // organic chemestry or something that I could not find the blog post on.
--- 2015/Day 20/Program.cs
+++ 2015/Day 20/Program.cs
@@ -1,47 +1,31 @@
-﻿static List<int> Factor(int number)
-{
-    // From StackOverflow. 
-    // https://stackoverflow.com/questions/239865/best-way-to-find-all-factors-of-a-given-number
-
-    var factors = new List<int>();
-    int max = (int)Math.Sqrt(number);  // Round down
-
-    for (int factor = 1; factor <= max; ++factor) // Test from 1 to the square root, or the int below it, inclusive.
-    {
-        if (number % factor == 0)
-        {
-            factors.Add(factor);
-            if (factor != number / factor) // Don't add the square root twice!  Thanks Jon
-                factors.Add(number / factor);
-        }
-    }
-    return factors;
-}
+﻿using BKH.EnumExtentions;
 
 try
 {
-    const int targetPresents = 33100000;
-    const int presentsPerElfPart1 = 10;
-    const int presentsPerElfPart2 = 11;
-    
-    int houseNumber = 0;
-    int houseNumberPart1 = 0;
-    int houseNumberPart2 = 0;
+    const long PPE_Part1 = 10; //Presents Per Elf
+    const long PPE_Part2 = 11;
 
-    while (true)
+    const string PUZZLE_INPUT = "PuzzleInput.txt";
+    long puzzleInput = long.Parse(File.ReadAllText(PUZZLE_INPUT));
+
+    long part1Answer = -1;
+    long part2Answer = -1;
+
+    for(long houseNumber = 0; part1Answer == -1 || part2Answer == -1; houseNumber++)
     {
-        houseNumber++;
-        var f = Factor(houseNumber);
+        IEnumerable<long> f = houseNumber.Factor();
 
-        if (houseNumberPart1 == 0) if (f.Sum() * presentsPerElfPart1 >= targetPresents) houseNumberPart1 = houseNumber;
-        if (houseNumberPart2 == 0) if (f.Where(x => x > houseNumber / 50).Sum() * presentsPerElfPart2 >= targetPresents) houseNumberPart2 = houseNumber;
+        if (part1Answer == -1)
+            if (f.Sum() * PPE_Part1 >= puzzleInput)
+                part1Answer = houseNumber;
 
-        if (houseNumberPart1 > 0 &&  houseNumberPart2 > 0) break;
+        if (part2Answer == -1)
+            if (f.Where(x => x > houseNumber / 50).Sum() * PPE_Part2 >= puzzleInput)
+                part2Answer = houseNumber;
     }
 
-    Console.WriteLine($"Part 1: The first house to get more than {targetPresents} is: {houseNumberPart1}.");
-
-    Console.WriteLine($"Part 2: After changing to only delivering to 50 houses, the first house to get more than {targetPresents} is: {houseNumberPart2}.");
+    Console.WriteLine($"Part 1: The first house to get more than {puzzleInput} is: {part1Answer}.");
+    Console.WriteLine($"Part 2: After changing to only delivering to 50 houses, the first house to get more than {puzzleInput} is: {part2Answer}.");
 }
 catch (Exception e)
 {
