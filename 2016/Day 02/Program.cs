cd525dc27fc23cb008cc7044ba6750377d0ac68a
Kezzryn <bkholmes@shaw.ca>
2024-06-07 11:39:07 -0500

    2016 Day 01, 02 Reworked with Point2D.

--- 2016/Day 02/Program.cs
+++ 2016/Day 02/Program.cs
@@ -1,97 +1,58 @@
-﻿using System.Drawing;
+﻿using BKH.AoC_Point2D;
 
-const char NULL_CHAR = '\0';
-const char START_CHAR = '5';
-
-string KeypadSolver(List<string> input, char[,] keypad)
+static string KeypadSolver(List<string> input, Dictionary<Point2D, char> keypad)
 {
-    Dictionary<Directions, Size> step = new()
-    {
-        { Directions.Up,    new ( 0,  1) } , 
-        { Directions.Down,  new ( 0, -1) } ,
-        { Directions.Left,  new (-1, 0) } , 
-        { Directions.Right, new ( 1, 0) } , 
-    };
+    Point2D cursor = Point2D.Origin;    //the keypads are "centered" on 0,0.
 
-    Point cursor = new(0, 0);
-
-    // find our start point, '5'
-    bool breakCheck = true;
-    for(int x = 0; x < keypad.GetUpperBound(0) && breakCheck; x++)
-    {
-        for (int y= 0; y < keypad.GetUpperBound(1) && breakCheck; y++)
-        {
-            if (keypad[x,y] == START_CHAR)
-            {
-                cursor = new(x,y);
-                breakCheck = false;
-            }
-        }
-    }
-
-    string answer = "";
+    string returnValue = "";
     foreach (string instruction in input)
     {
         foreach (char direction in instruction)
         {
-            switch (direction)
+            Point2D nextStep = direction switch
             {
-                case 'U':
-                    if (cursor.Y == keypad.GetUpperBound(1) || (keypad[cursor.X, cursor.Y + step[Directions.Up].Height] == NULL_CHAR)) continue;
-                    cursor += step[Directions.Up];
-                    break;
-                case 'D':
-                    if (cursor.Y == keypad.GetLowerBound(1) || (keypad[cursor.X, cursor.Y + step[Directions.Down].Height] == NULL_CHAR)) continue;
-                    cursor += step[Directions.Down];
-                    break;
-                case 'R':
-                    if (cursor.X == keypad.GetUpperBound(0) || (keypad[cursor.X + step[Directions.Right].Width, cursor.Y] == NULL_CHAR)) continue;
-                    cursor += step[Directions.Right];
-                    break;
-                case 'L':
-                    if (cursor.X == keypad.GetLowerBound(0) || (keypad[cursor.X + step[Directions.Left].Width, cursor.Y] == NULL_CHAR)) continue;
-                    cursor += step[Directions.Left];
-                    break;
-                default:
-                    throw new NotImplementedException();
+                'U' => cursor.OrthogonalNeighbor(Point2D.Direction.Up),
+                'D' => cursor.OrthogonalNeighbor(Point2D.Direction.Down),
+                'L' => cursor.OrthogonalNeighbor(Point2D.Direction.Left),
+                'R' => cursor.OrthogonalNeighbor(Point2D.Direction.Right),
+                _ => throw new NotImplementedException()
             };
-        }
-        answer += keypad[cursor.X, cursor.Y];
-    }
-    return answer; 
-}
 
+            if(keypad.ContainsKey(nextStep)) cursor = nextStep;
+        }
+        returnValue += keypad[cursor];
+    }
+    return returnValue; 
+}
 
 try
 {
     const string PUZZLE_INPUT = "PuzzleInput.txt";
-    List<string> puzzleInput = File.ReadAllLines(PUZZLE_INPUT).ToList();
-    //List<string> puzzleInput = new() { "ULL", "RRDDD", "LURDL", "UUUUD" };
+    List<string> puzzleInput = [.. File.ReadAllLines(PUZZLE_INPUT)];
 
-    char[,] keypadPart1 =
+    Dictionary<Point2D, char> keypadPart1 = new()
     {
-        { '7', '4', '1' },
-        { '8', '5', '2' },
-        { '9', '6', '3' } 
+        { new(-1, 1), '1' }, { new(0, 1), '2' }, { new(1, 1), '3' },
+        { new(-1, 0), '4' }, { new(0, 0), '5' }, { new(1, 0), '6' },
+        { new(-1,-1), '7' }, { new(0,-1), '8' }, { new(1,-1), '9' },
     };
 
-    char[,] keypadPart2 = {
-        { NULL_CHAR, NULL_CHAR, '5', NULL_CHAR, NULL_CHAR},
-        { NULL_CHAR, 'A',       '6', '2',       NULL_CHAR },
-        { 'D',       'B',       '7', '3',       '1' },
-        { NULL_CHAR, 'C',       '8', '4',       NULL_CHAR },
-        { NULL_CHAR, NULL_CHAR, '9', NULL_CHAR, NULL_CHAR }
+    Dictionary<Point2D, char> keypadPart2 = new()
+    {
+                                                  { new( 2, 2), '1' },
+                             { new( 1, 1), '2' }, { new( 2, 1), '3' }, { new( 3, 1), '4' },
+        { new( 0, 0), '5' }, { new( 1, 0), '6' }, { new( 2, 0), '7' }, { new( 3, 0), '8' }, { new( 4, 0), '9' },
+                             { new( 1,-1), 'A' }, { new( 2,-1), 'B' }, { new( 3,-1), 'C' },
+                                                  { new( 2,-2), 'D' }
     };
 
     string part1Answer = KeypadSolver(puzzleInput, keypadPart1);
     string part2Answer = KeypadSolver(puzzleInput, keypadPart2);
 
     Console.WriteLine($"Part 1: The bathroom door code is: {part1Answer}");
-    Console.WriteLine($"Part 1: The insane bathroom door code is: {part2Answer}");
+    Console.WriteLine($"Part 2: The insane bathroom door code is: {part2Answer}");
 }
 catch (Exception e)
 {
     Console.WriteLine(e);
 }
-
-enum Directions  {  Up, Down, Right, Left }
