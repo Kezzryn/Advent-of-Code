68c6b592b51d7d8fc8cbb117e38920a68eee88e5
Kezzryn <bkholmes@shaw.ca>
2024-06-07 22:00:01 -0500

    2015 Day 10 reworked.

--- 2015/Day 10/Program.cs
+++ 2015/Day 10/Program.cs
@@ -1,44 +1,46 @@
 ﻿using System.Text;
 
+static string SpeakAndSay(string input)
+{
+    StringBuilder sb = new();
+
+    int i = 0;
+    while (i < input.Length)
+    {
+        char currChar = input[i];
+        int nextIndex = input.IndexOfAny("1234567890".Except([currChar]).ToArray(), i);
+
+        string chunk = (nextIndex == -1) ? input[i..] : input[i..nextIndex];
+
+        sb.Append(chunk.Length);
+        sb.Append(currChar);
+
+        i = (nextIndex == -1) ? input.Length : nextIndex;
+    }
+
+    return sb.ToString();
+}
+
 try
 {
-    const string PUZZLE_INPUT = "PuzzleInput.txt";
     const int PART1_NUM_LOOPS = 40;
     const int MAX_LOOPS = 50;
 
-    string puzzleInputPart1 = File.ReadAllText(PUZZLE_INPUT);
-    string puzzleInputPart2 = File.ReadAllText(PUZZLE_INPUT);
+    const string PUZZLE_INPUT = "PuzzleInput.txt";
+    string puzzleInput = File.ReadAllText(PUZZLE_INPUT);
 
-    string SpeakAndSay(string input)
-    {
-        StringBuilder sb = new();
-
-        int i = 0;
-        while (i < input.Length)
-        {
-            char currChar = input[i];
-            int nextIndex = input.IndexOfAny("1234567890".Except(new char[] { currChar }).ToArray(), i);
-
-            string chunk = (nextIndex == -1) ? input[i..] : input[i..nextIndex];
-
-            sb.Append(chunk.Length);
-            sb.Append(currChar);
-
-            i = (nextIndex == -1) ? input.Length : nextIndex;
-        }
-
-        return sb.ToString(); 
-    }
-
+    int part1Answer = 0;
+    
     for (int i = 1; i <= MAX_LOOPS; i++ )
     {
-        if (i <= PART1_NUM_LOOPS) puzzleInputPart1 = SpeakAndSay(puzzleInputPart1);
-        puzzleInputPart2 = SpeakAndSay(puzzleInputPart2);
+        puzzleInput = SpeakAndSay(puzzleInput);
+        if (i == PART1_NUM_LOOPS) part1Answer = puzzleInput.Length;
     }
-    
-    Console.WriteLine($"Part 1: The length of the result after {PART1_NUM_LOOPS} iterations is {puzzleInputPart1.Length} .");
-    Console.WriteLine($"Part 2: The length of the result after {MAX_LOOPS} iterations is {puzzleInputPart2.Length} .");
 
+    int part2Answer = puzzleInput.Length;
+
+    Console.WriteLine($"Part 1: The length of the result after {PART1_NUM_LOOPS} iterations is {part1Answer}.");
+    Console.WriteLine($"Part 2: The length of the result after {MAX_LOOPS} iterations is {part2Answer}.");
 }
 catch (Exception e)
 {
