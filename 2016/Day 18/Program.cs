3edc273c5cd8513dc5aa6b067744a7670e505899
Kezzryn <bkholmes@shaw.ca>
2024-06-20 20:07:52 -0500

    2016 Day 18 net80, optimization

--- 2016/Day 18/Program.cs
+++ 2016/Day 18/Program.cs
@@ -1,51 +1,28 @@
-﻿using System.Text;
-
-try
+﻿try
 {
     const int ROWS_PART_1 = 40;
     const int ROWS_PART_2 = 400000;
 
     const string PUZZLE_INPUT = "PuzzleInput.txt";
-    string puzzleInput = File.ReadAllText(PUZZLE_INPUT);
+    bool[] puzzleInput = File.ReadAllText(PUZZLE_INPUT).Select(x => x == '.').ToArray();
 
-    static int solve(int rows, string firstLine)
+    int part1Answer = 0;
+    int part2Answer = puzzleInput.Count(x => x);
+
+    for (int i = 2; i <= ROWS_PART_2; i++)
+    // start at 2 to account for priming read. 
     {
-        const char safeSpace = '.';
-        const char trapSpace = '^';
-        int row_length = firstLine.Length;
+        puzzleInput = puzzleInput.Select((x, j) => ((j == 0) || puzzleInput[j - 1]) == ((j == puzzleInput.Length - 1) || puzzleInput[j + 1])).ToArray();
 
-        List<string> puzzle = new()
-        {
-            firstLine
-        };
+        part2Answer += puzzleInput.Count(x => x);
 
-        for (int i = 0; puzzle.Count < rows; i++)
-        {
-            StringBuilder newRow = new();
-
-            string lastRow = puzzle.Last(); // performance cache
-            
-            for (int j = 0; j < row_length; j++)
-            {
-                char left = (j == 0) ? safeSpace : lastRow[j - 1];
-                char right = (j == row_length - 1) ? safeSpace : lastRow[j + 1];
-
-                newRow.Append(left == right ? safeSpace : trapSpace);
-            }
-            puzzle.Add(newRow.ToString());
-        }
-
-        return puzzle.Select(x => x.ToCharArray().Count(c => c == safeSpace)).Sum();
+        if (i == ROWS_PART_1) part1Answer = part2Answer;
     }
 
-    int part1Answer = solve(ROWS_PART_1, puzzleInput);
     Console.WriteLine($"Part 1: The number of safe spaces in {ROWS_PART_1} is {part1Answer}.");
 
-    int part2Answer = solve(ROWS_PART_2, puzzleInput);
     Console.WriteLine($"Part 2: The number of safe spaces in {ROWS_PART_2} is {part2Answer}.");
-
 }
-
 catch (Exception e)
 {
     Console.WriteLine(e);
