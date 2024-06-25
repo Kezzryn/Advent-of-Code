f14d6b1e7aff0ac072225b4ccd5c5e13039e384c
Kezzryn <bkholmes@shaw.ca>
2024-06-25 08:41:04 -0500

    2022 Day 25 converted to use Base10Converter library

--- 2022/Day 25/Program.cs
+++ 2022/Day 25/Program.cs
@@ -1,16 +1,15 @@
-﻿using AoC_2022_Day_25;
+﻿using BKH.Base10Converter;
 
 try
 {
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string[] puzzleInput = File.ReadAllLines(PUZZLE_INPUT);
 
-    Base10Converter base10Converter = new();
+    string part1Answer = Base10Converter.FromBase10(
+                    puzzleInput.Sum(snafu => Base10Converter.ToBase10(snafu, TranslationMap.Maps["SNAFU"])),
+                    TranslationMap.Maps["SNAFU"]);
 
-    Console.WriteLine($"The snafu number is {base10Converter.ConvertFromBase10(
-                puzzleInput.Sum(
-                    snafu => base10Converter.ConvertToBase10(snafu, "Snafu"))
-                , "Snafu")}");
+     Console.WriteLine($"The snafu number is {part1Answer}");
 }
 catch (Exception e)
 {
