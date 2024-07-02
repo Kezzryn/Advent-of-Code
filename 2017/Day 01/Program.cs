e2b017cfaeba4d772d9b2b6d83f2cded06e8efad
Kezzryn <bkholmes@shaw.ca>
2024-07-02 16:42:43 -0500

    2017 Day 1-2 .net8 and LINQify

--- 2017/Day 01/Program.cs
+++ 2017/Day 01/Program.cs
@@ -1,34 +1,13 @@
 ﻿try
 {
     const string PUZZLE_INPUT = "PuzzleInput.txt";
-    string puzzleInput = File.ReadAllText(PUZZLE_INPUT);
+    char[] puzzleInput = File.ReadAllText(PUZZLE_INPUT).ToCharArray();
 
-    int part1Answer = 0;
-    int part2Answer = 0;
-
-
-    for (int i = 0; i < puzzleInput.Length; i++)
-    {
-        // use modulo math to wrap when we hit the end, or when finding the midpoint. 
-        int nextChar = (i + 1) % puzzleInput.Length;
-
-        if (puzzleInput[i] == puzzleInput[nextChar]) 
-        {
-            part1Answer += int.Parse(puzzleInput[i].ToString());
-        }
-
-        nextChar = (i + (puzzleInput.Length /2)) % puzzleInput.Length;
-
-        if (puzzleInput[i] == puzzleInput[nextChar])
-        {
-            part2Answer += int.Parse(puzzleInput[i].ToString());
-        }
-    }
+    int part1Answer = puzzleInput.Where((x, i) => x == puzzleInput[i + 1 >= puzzleInput.Length ? 0 : i + 1]).Sum(x => x - '0');
+    int part2Answer = puzzleInput.Where((x, i) => x == puzzleInput[(i + (puzzleInput.Length / 2)) % puzzleInput.Length]).Sum(x => x - '0');
 
     Console.WriteLine($"Part 1: The captcha answer is: {part1Answer}.");
-
     Console.WriteLine($"Part 2: The captcha of halfway digit is {part2Answer}.");
-
 }
 catch (Exception e)
 {
