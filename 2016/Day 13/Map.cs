2ca33619e7cf9619447231fcbb83c952cefd724b
Kezzryn <bkholmes@shaw.ca>
2024-07-03 21:34:43 -0500

    2016 Day 13 .net8 Use BaseMap

--- 2016/Day 13/Map.cs
+++ 2016/Day 13/Map.cs
@@ -1,186 +1,61 @@
-﻿using System.Drawing;
+﻿namespace AoC_2016_Day_13;
+using BKH.BaseMap;
+using BKH.Geometry;
+
 using System.Numerics;
 
-namespace AoC_2016_Day_13
+internal class Map : BaseMap
 {
-    internal class Map
+    private const int WALL = 1;
+    private const int AIR = 0;
+
+    // puzzle magic number for maze generation.
+    private readonly int _designer_number = 0;
+
+    private int GenMapPoint(Point2D p) => GenMapPoint(p.X, p.Y);
+
+    private int GenMapPoint(int x, int y) => int.IsEvenInteger(BitOperations.PopCount((uint)((x * x) + (3 * x) + (2 * x * y) + y + (y * y) + _designer_number))) ? AIR : WALL;
+
+    public bool IsValidMapPoint(int x, int y) => IsValidMapPoint(new Point2D(x, y));
+    public bool IsValidMapPoint(Point2D point)
     {
-        private readonly Point _defaultStart = new(1, 1);
-        private readonly Point _defaultEnd = new(31, 39);
-
-        public Point MapMin => new (_finalPath.Select(k => k.X).Min(), _finalPath.Select(k => k.Y).Min());
-        public Point MapMax => new (_finalPath.Select(k => k.X).Max(), _finalPath.Select(k => k.Y).Max());
-
-        // the path after an A_Star call. 
-        public List<Point> _finalPath = new();
-
-        // puzzle magic number for maze generation.
-        private readonly int _designer_number;
-
-        public Map(int designer_number) 
+        if (!_theMap.TryGetValue(point, out int mapValue))
         {
-            _designer_number = designer_number;
+            mapValue = GenMapPoint(point);
+            _theMap[point] = mapValue;
         }
+        return mapValue == AIR;
+    }
 
-        public Point DefaultStart() => _defaultStart;
+    public Map(int designerNumber, Point2D defaultStart, Point2D defaultEnd)
+    {
+        _designer_number = designerNumber;
+        _theMap.Add(defaultStart, GenMapPoint(defaultStart));
+        _theMap.Add(defaultEnd, GenMapPoint(defaultEnd));
+        StartPosition = defaultStart;
+        EndPosition = defaultEnd;
+    }
 
-        public Point DefaultEnd() => _defaultEnd;
+    protected override int TestStep(Point2D cursor, Point2D nextStep)
+    {
+        //Don't move to ourself. 
+        if (cursor == nextStep) return -1;
 
-        private bool GenMap(Point p) => GenMapPoint(p.X, p.Y);
+        //we're off the map
+        if (nextStep.X < 0 || nextStep.Y < 0) return -1;
 
-        public bool GenMapPoint(int x, int y)
-        {
-            uint v = (uint)((x * x) + (3 * x) + (2 * x * y) + y + (y * y) + _designer_number);
-            return (BitOperations.PopCount(v) % 2) == 0;
-            /*
-            // This is bonkers. I do not understand it, but it works.
-            // Also this is the same algrothm implemented in the software fallback of the PopCount function.
-            // http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
-            // The best method for counting bits in a 32 - bit integer v is the following:
+        //we'll exceed our step count (if applicable) 
+        if (MaxSteps != 0 && stepCounter[cursor].gScore + 1 >= MaxSteps) return -1;
 
-            v -= ((v >> 1) & 0x55555555);                    // reuse input as temporary
-            v = (v & 0x33333333) + ((v >> 2) & 0x33333333);     // temp
-            int c = ((v + (v >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
+        // check our map coordinate for validity.
+        if (!IsValidMapPoint(nextStep.X, nextStep.Y)) return -1; 
             
-            return (c % 2) == 0;
-            */
-        }
+        stepCounter.TryAdd(nextStep, (int.MaxValue, int.MaxValue, null));
+        return 1;
+    }
 
-        public static int TaxiDistance(Point a, Point b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
-
-        public bool A_Star(Point start, Point dest, out int numSteps, int? maxSteps = null)
-        {
-            _finalPath.Clear();
-
-            List<Size> neighbors = new()
-            {
-                new Size(1,0),
-                new Size(-1,0),
-                new Size(0,1),
-                new Size(0,-1)
-            };
-
-            PriorityQueue<Point, int> searchQueue = new(); //we enque based on fScore + h, the distance travelled, plus taxi distance guess to destination.
-            HashSet<Point> inSearchQueue = new(); //we add this because we don't have a way to query the queue to see if a specific item is in it.
-
-            //gScore is value of the path from start to here
-            //fScore is estimated from here to end. We're using Taxi Distance as our hieruistic 
-            Dictionary<Point, (int gScore, int fScore, Point? parent)> stepCounter = new()
-            {
-                { start, (0, TaxiDistance(start, dest), null) }
-            };
-
-            bool TestStep(Point cursor, Point nextPoint)
-            {
-                //Don't move to ourself. 
-                if (cursor == nextPoint) return false;
-
-                //we're off the map
-                if (nextPoint.X < 0 || nextPoint.Y < 0) return false;
-
-                //we'll exceed our step count (if applicable) 
-                if (maxSteps != null && stepCounter[cursor].gScore + 1 > maxSteps) return false;
-
-                // check our map coordinate for validity.
-                if (GenMap(nextPoint))
-                {
-                    if (!stepCounter.ContainsKey(nextPoint))
-                    {
-                        stepCounter.Add(nextPoint, (int.MaxValue, int.MaxValue, null));
-                    }
-                    return true;
-                }
-
-                return false; //default to nope. 
-            }
-
-            searchQueue.Enqueue(start, TaxiDistance(start, dest));
-            inSearchQueue.Add(start);
-
-            while (searchQueue.Count > 0)
-            {
-                Point cursor = searchQueue.Dequeue();
-                inSearchQueue.Remove(cursor);
-
-                //We have arrived!
-                if (cursor == dest)
-                {
-                    numSteps = stepCounter[cursor].gScore;
-
-                    //unroll our history. 
-                    _finalPath.Add(cursor);
-                    Point? p = stepCounter[cursor].parent;
-
-                    while (p != null)
-                    {
-                        _finalPath.Add((Point)p);
-                        p = stepCounter[(Point)p].parent;
-                    }
-
-                    return true;
-                }
-
-                foreach (Size neighbor in neighbors)
-                {
-                    Point nextStep = cursor + neighbor;
-
-                    //bounds and valid move check. 
-                    if (TestStep(cursor, nextStep))
-                    {
-                        //tentative_gScore := gScore[current] + d(current, neighbor)
-                        int t_gScore = stepCounter[cursor].gScore + 1;
-
-                        //if tentative_gScore < gScore[neighbor]
-                        if (t_gScore < stepCounter[nextStep].gScore)
-                        {
-                            //cameFrom[neighbor] := current
-                            //gScore[neighbor] := tentative_gScore
-                            //fScore[neighbor] := tentative_gScore + h(neighbor)
-                            stepCounter[nextStep] = (t_gScore, t_gScore + TaxiDistance(nextStep, dest), cursor);
-
-                            //if neighbor not in openSet openSet.add(neighbor) 
-                            if (!inSearchQueue.Contains(nextStep))
-                            {
-                                searchQueue.Enqueue(nextStep, stepCounter[nextStep].fScore);
-                                inSearchQueue.Add(nextStep);
-                            }
-                        }
-                    }
-                }
-            }
-            numSteps = -1;
-            return false;
-        }
-
-        public void DisplayMap() => DisplayMap(_finalPath);
-
-        public void DisplayMap(List<Point> drawPath)
-        {
-            Point cursor = new();
-
-            for (int y = 0; y <= MapMax.Y+1; y++)
-            {
-                for (int x = 0; x <= MapMax.X + 1; x++)
-                {
-                    cursor.X = x;
-                    cursor.Y = y;
-                    char c = GenMapPoint(x, y) ? '.' : '#';
-
-                    if (drawPath.Contains(cursor))
-                    {
-                        c = 'x';
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                    }
-                    if (cursor == _defaultStart) Console.ForegroundColor = ConsoleColor.Green;
-                    if (cursor == _defaultEnd) Console.ForegroundColor = ConsoleColor.Blue;
-
-                    Console.Write(c);
-
-                    if (Console.ForegroundColor != ConsoleColor.Gray) Console.ForegroundColor = ConsoleColor.Gray;
-                }
-                Console.WriteLine(" ");
-            }
-        }
-
+    protected override IEnumerable<Point2D> NextSteps(Point2D cursor)
+    {
+        return cursor.GetOrthogonalNeighbors();
     }
 }
--- 2016/Day 13/Program.cs
+++ 2016/Day 13/Program.cs
@@ -1,31 +1,37 @@
 ﻿using AoC_2016_Day_13;
-using System.Drawing;
+using BKH.Geometry;
 
 try
 {
-    const string PUZZLE_INPUT = "PuzzleInput.txt";
     const int PART2_STEPS = 50;
+
+    const string PUZZLE_INPUT = "PuzzleInput.txt";
     int puzzleInput = File.ReadAllLines(PUZZLE_INPUT).Select(int.Parse).FirstOrDefault();
 
-    Map theMap = new(puzzleInput);
-    theMap.A_Star(theMap.DefaultStart(), theMap.DefaultEnd(), out int numSteps);
+    Map theMap = new(puzzleInput, new(1, 1), new(31, 39));
+
+    theMap.A_Star();
     
-    Console.WriteLine($"Part 1: The shortest distance from {theMap.DefaultStart()} to {theMap.DefaultEnd()} is {numSteps}.");
+    Console.WriteLine($"Part 1: The shortest distance from {theMap.StartPosition} to {theMap.EndPosition} is {theMap.NumSteps}.");
     theMap.DisplayMap();
 
     int part2Answer = 0;
-    
-    for (int x = 0; x <= PART2_STEPS + theMap.DefaultStart().X; x++)
+    theMap.MaxSteps = PART2_STEPS;
+
+    for (int x = 0; x <= PART2_STEPS + theMap.StartPosition.X; x++)
     {
-        for (int y = 0; y <= PART2_STEPS + theMap.DefaultStart().Y; y++)
+        for (int y = 0; y <= PART2_STEPS + theMap.StartPosition.Y; y++)
         {
-            if (theMap.GenMapPoint(x, y) && theMap.A_Star(theMap.DefaultStart(), new Point(x, y), out numSteps, PART2_STEPS)) part2Answer++;
+            if (theMap.IsValidMapPoint(x, y)) 
+            {
+                theMap.EndPosition = new Point2D(x, y);
+                if (theMap.A_Star()) part2Answer++;
+            }
         }
     }
 
-    Console.WriteLine($"Part 2: The number of reachable positions within {PART2_STEPS} from the {theMap.DefaultStart()} is {part2Answer}.");
+    Console.WriteLine($"Part 2: The number of reachable positions within {PART2_STEPS} from the {theMap.StartPosition} is {part2Answer}.");
 }
-
 catch (Exception e)
 {
     Console.WriteLine(e);
