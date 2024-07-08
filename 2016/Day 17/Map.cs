6cce036f0c545e2ca4b807c47fc12dad10887708
Kezzryn <bkholmes@shaw.ca>
2024-07-08 00:53:47 -0500

    2016 Day 17 Heavily reworked to use BaseMap3D.

--- 2016/Day 17/Map.cs
+++ 2016/Day 17/Map.cs
@@ -1,327 +1,158 @@
-﻿using System.Drawing;
+﻿namespace AoC_2016_Day_17;
+using BKH.Geometry;
+using BKH.BaseMap;
+using System.Security.Cryptography;
+using System.Text;
 
-namespace AoC_2016_Day_17
+internal class Map : BaseMap3D
 {
-    internal class Map
+    private readonly Dictionary<Point3D, string> _history = [];     // Step tracker.
+    private readonly Dictionary<string, List<bool>> _MD5HashHistory = []; // MD5 hash results cache.
+    public string History { get; protected set; } = String.Empty;
+    private readonly string _secretHash;
+
+    public Map(string puzzleInput)
     {
-        // this serves as our reference starting position for everything. 
-        private readonly Dictionary<Point, int> _theMap = new();
-        private readonly Dictionary<Point3D, string> _history = new();
+        for (int x = 0; x < 4; x++)
+            for (int y = 0; y < 4; y++)
+                _theMap.Add(new Point3D(x,y,0), 1);
 
-        //as found on the map.
-        private readonly Point3D _defaultStart = new(0, 3, 0);
-        private readonly Point3D _defaultEnd = new(3, 0, 0);
+        _secretHash = puzzleInput;
 
-        public Point MapMin { get; }
+        //Using the Z axis in the step counters, so we can visit the "same" room again.
+        StartPosition = new(0, 3, 0);   //upper left, to bottom right. 
+        EndPosition = new(3, 0, 0);
+    }
 
-        public Point MapMax { get; }
+    protected override int TestStep(Point3D current, Point3D nextPoint)
+    {
+        // Off the map
+        if (!_theMap.TryGetValue(new(nextPoint.As2D()), out int stepLength)) return -1;
+        if (!_history.TryGetValue(current, out string? historyValue)) historyValue = String.Empty;
 
-        //the path after an A_Star call. 
-        public List<Point3D> FinalPath = new();
+        string dirValue = String.Empty;
+        if (IsDoor(historyValue, Direction.RIGHT) && current.X < nextPoint.X) dirValue = "R";
+        if (IsDoor(historyValue, Direction.LEFT)  && current.X > nextPoint.X) dirValue = "L";
+        if (IsDoor(historyValue, Direction.UP)    && current.Y < nextPoint.Y) dirValue = "U";
+        if (IsDoor(historyValue, Direction.DOWN)  && current.Y > nextPoint.Y) dirValue = "D";
+        if (dirValue == String.Empty) return -1;
 
-        public string History = String.Empty;
-        
-        private readonly string _secretHash;
-
-        public Map(string puzzleInput)
+        if (!_history.TryAdd(nextPoint, $"{historyValue}{dirValue}"))
         {
-            for (int x = 0; x < 4; x++)
-                for (int y = 0; y < 4; y++)
-                    _theMap.Add(new Point(x,y), 1);
-
-            _secretHash = puzzleInput;
-
-            MapMin = new Point(_theMap.Keys.Select(k => k.X).Min(), _theMap.Keys.Select(k => k.Y).Min());
-            MapMax = new Point(_theMap.Keys.Select(k => k.X).Max(), _theMap.Keys.Select(k => k.Y).Max());
+            string temp = $"{historyValue}{dirValue}";
+            if (temp.Length > _history[nextPoint].Length) _history[nextPoint] = temp;
         }
 
-        public static int TaxiDistance(Point s, Point e)
+        stepCounter.TryAdd(nextPoint, (int.MaxValue, int.MaxValue, null));
+        return stepLength;
+    }
+
+    protected override int Heuristic(Point3D a, Point3D b)
+    {
+        return Point3D.TaxiDistance2D(a, b);
+    }
+
+    protected override bool IsAtExit(Point3D a)
+    {
+        if (a.AsPoint2D() == EndPosition.AsPoint2D())
         {
-            return Math.Abs(s.X - e.X) + Math.Abs(s.Y - e.Y);
-        }
-
-        public Point3D DefaultStart => _defaultStart;
-
-        public Point3D DefaultEnd => _defaultEnd;
-
-        private bool TestStep(Point3D current, Point3D nextPoint)
-        {
-            const int UP = 0;
-            const int DOWN = 1;
-            const int LEFT = 2;
-            const int RIGHT = 3;
-
-            // Off the map
-            if (!_theMap.TryGetValue(nextPoint.Get2DPoint(), out _ )) return false;
-
-            string histValue = _history.TryGetValue(current, out string? historyValue) ? historyValue : String.Empty;
-            string hash = MD5Hasher.GetHash($"{_secretHash}{histValue}");
-
-            // 0U 1D 2L 3R 
-            bool[] dirs = new bool[4];
-
-            for(int i = 0; i < 4; i++)
-            {
-                dirs[i] = "bcdef".Contains(hash[i]);
-            }
-
-            string dirValue = String.Empty;
-
-            if (current.X < nextPoint.X) 
-            {
-                if (!dirs[RIGHT]) return false;
-                dirValue = "R";
-            }
-
-            if (current.X > nextPoint.X)
-            {
-                if (!dirs[LEFT]) return false;
-                dirValue = "L";
-            }
-
-            if (current.Y < nextPoint.Y)
-            {
-                if (!dirs[UP]) return false;
-                dirValue = "U";
-            }
-
-            if (current.Y > nextPoint.Y)
-            {
-                if (!dirs[DOWN]) return false;
-                dirValue = "D";
-            }
-
-            if (!_history.TryAdd(nextPoint, $"{histValue}{dirValue}"))
-            {
-                string temp = $"{histValue}{dirValue}";
-                if (temp.Length > _history[nextPoint].Length) _history[nextPoint] = temp;
-            }
+            History = _history[a];
             return true;
         }
+        return false; 
+    }
 
-        public bool A_Star(Point3D start, Point3D dest, out int numSteps, bool longPath = false)
+    protected override IEnumerable<Point3D> NextSteps(Point3D cursor)
+    {
+        foreach(Point2D n in cursor.AsPoint2D().GetOrthogonalNeighbors())
         {
-            FinalPath.Clear();
+            yield return new Point3D(n.X, n.Y, cursor.Z + 1);
+        }
+    }
 
-            int maxSteps = -1;
-            _history.Clear();
-
-            List<Point3D> neighbors = new()
-            {
-                new Point3D(1,0,1), // right
-                new Point3D(-1,0,1),// left
-                new Point3D(0,1,1), // up
-                new Point3D(0,-1,1)// down
-                //new Point3D(0,0,1)  // stay
-            };
-
-            PriorityQueue<Point3D, int> searchQueue = new(); //we enque based on fScore + h, the distance travelled, plus taxi distance guess to destination.
-            HashSet<Point3D> inSearchQueue = new(); //we add this because we don't have a way to query the queue to see if a specific item is in it.
-
-            //gScore is value of the path from start to here
-            //fScore is estimated from here to end. We're using Taxi Distance as our hieruistic 
-            Dictionary<Point3D, (int gScore, int fScore, Point3D? parent)> stepCounter = new()
-            {
-                { start, (0, TaxiDistance(start.Get2DPoint(), dest.Get2DPoint()), null) }
-            };
-
-            _history.Add(start, "");
-
-            searchQueue.Enqueue(start, TaxiDistance(start.Get2DPoint(), dest.Get2DPoint()));
-            inSearchQueue.Add(start);
-
-            while (searchQueue.Count > 0)
-            {
-                Point3D cursor = searchQueue.Dequeue();
-                inSearchQueue.Remove(cursor);
-
-                //We have arrived!
-                if (cursor.Get2DPoint() == dest.Get2DPoint())
-                {
-                    numSteps = stepCounter[cursor].gScore;
-                    if (numSteps > maxSteps) maxSteps = numSteps;
-
-                    //unroll our history. 
-                    FinalPath.Add(cursor);
-                    Point3D? p = stepCounter[cursor].parent;
-
-                    while (p != null)
-                    {
-                        FinalPath.Add(p);
-                        p = stepCounter[p].parent;
-                    }
-
-                    History = _history[cursor].ToString();
-
-                    if (!longPath) return true;
-                }
-
-                foreach (Point3D neighbor in neighbors)
-                {
-                    Point3D nextStep = cursor + neighbor;
-
-                    //bounds and valid move check. 
-                    if (TestStep(cursor, nextStep))
-                    {
-                        if (!stepCounter.ContainsKey(nextStep)) stepCounter.Add(nextStep, (int.MaxValue, int.MaxValue, null));
-
-                        //tentative_gScore := gScore[current] + d(current, neighbor)
-                        int t_gScore = stepCounter[cursor].gScore + 1;
-
-                        //if tentative_gScore < gScore[neighbor]
-                        if (t_gScore < stepCounter[nextStep].gScore || longPath)
-                        {
-                            //cameFrom[neighbor] := current
-                            //gScore[neighbor] := tentative_gScore
-                            //fScore[neighbor] := tentative_gScore + h(neighbor)
-                            stepCounter[nextStep] = (t_gScore, t_gScore + TaxiDistance(nextStep.Get2DPoint(), dest.Get2DPoint()), cursor);
-
-                            //if neighbor not in openSet openSet.add(neighbor) 
-                            if (!inSearchQueue.Contains(nextStep) || longPath)
-                            {
-                                searchQueue.Enqueue(nextStep, stepCounter[nextStep].fScore);
-                                inSearchQueue.Add(nextStep);
-                            }
-                        }
-                    }
-                }
-            }
-            if (longPath)
-            {
-                numSteps = maxSteps;
-                return true;
-            }
-            numSteps = -1;
-            return false;
+    private bool IsDoor(string locationKey, Direction direction)
+    {
+        string hashKey = $"{_secretHash}{locationKey}";
+        if(_MD5HashHistory.TryGetValue(hashKey, out List<bool>? history)) 
+        {
+            return history[(int)direction];
         }
 
-        public int A_Slow(Point3D start, Point3D dest)
+        byte[] hashdata = MD5.HashData(Encoding.UTF8.GetBytes(hashKey));
+        var sBuilder = new StringBuilder(); // Create a new Stringbuilder to collect the bytes and create a string.
+
+        // Loop through each byte of the hashed data and format each one as a hexadecimal string.
+        for (int i = 0; i < 4; i++)
         {
-            // stripped all the optimizations out of A_Start and returned the max value. 
-            int maxSteps = -1;
-            _history.Clear();
-            List<string> neighbors = new()
+            sBuilder.Append(hashdata[i].ToString("x2"));
+        }
+
+        List<bool> dirs = sBuilder.ToString().Select("bcdef".Contains).ToList(); // Return the hexadecimal string.
+        _MD5HashHistory.TryAdd(locationKey, new(dirs));
+
+        return _MD5HashHistory[locationKey][(int)direction];
+    }
+
+    public int A_Slow()
+    {
+        int maxSteps = -1;
+        Dictionary<Direction, char> neighbors = new()
+        { 
+            { Direction.UP, 'U' },
+            { Direction.DOWN, 'D' },
+            { Direction.LEFT, 'L' },
+            { Direction.RIGHT, 'R' },
+        };
+
+        Queue<string> searchQueue = new(); 
+
+        searchQueue.Enqueue(string.Empty);
+
+        while (searchQueue.TryDequeue(out string? cursor))
+        {
+            if (GetPointFromHistory(cursor) == EndPosition.AsPoint2D())
             {
-                "U","D","L","R"
-            };
-
-            Queue<string> searchQueue = new(); 
-
-            searchQueue.Enqueue(string.Empty);
-
-            while (searchQueue.Count > 0)
+                if (cursor.Length > maxSteps) maxSteps = cursor.Length;
+            }
+            else
             {
-                string cursor = searchQueue.Dequeue();
-
-                if (GetPointFromHistory(cursor, start) == dest.Get2DPoint())
+                foreach ((Direction dir, char neighbor) in neighbors)
                 {
-                    if( cursor.Length > maxSteps) maxSteps = cursor.Length;
-                    continue;
-                }
-
-                foreach (string neighbor in neighbors)
-                {
-                    //bounds and valid move check. 
-                    if (TestStepA_Slow(cursor, neighbor, start))
+                    string nextStep = cursor + neighbor;
+                    // Off the map
+                    if (_theMap.TryGetValue(GetPointFromHistory(nextStep), out _) && IsDoor(cursor, dir))
                     {
-                        string nextStep = cursor + neighbor;
                         searchQueue.Enqueue(nextStep);
                     }
                 }
             }
-            return maxSteps;
         }
+        return maxSteps;
+    }
 
-        private static Point GetPointFromHistory(string history, Point3D start)
+    private Point3D GetPointFromHistory(string history)
+    {
+        //Coverts a history string into a point.
+        //Used in A_Slow to test if we've hit the end.
+        int x = StartPosition.X;
+        int y = StartPosition.Y;
+
+        foreach(var gb in history.ToCharArray().GroupBy(x => x))
         {
-            //Coverts a history string into a point.  used in A_Slow to test if they've hit the end.
-            int x = start.X;
-            int y = start.Y;
-            foreach (char c in history)
-            {
-                switch (c)
-                {
-                    case 'U':
-                        y++;
-                        break;
-                    case 'D':
-                        y--;
-                        break;
-                    case 'L':
-                        x--;
-                        break;
-                    case 'R':
-                        x++;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            return new Point(x, y);
+            if (gb.Key == 'U') y += gb.Count();
+            if (gb.Key == 'D') y -= gb.Count();
+            if (gb.Key == 'R') x += gb.Count();
+            if (gb.Key == 'L') x -= gb.Count();
         }
 
-        private bool TestStepA_Slow(string current, string nextPoint, Point3D start)
-        {
+        return new Point3D(x, y, 0);
+    }
 
-            const int UP = 0;
-            const int DOWN = 1;
-            const int LEFT = 2;
-            const int RIGHT = 3;
-
-            // Off the map
-            if (!_theMap.TryGetValue(GetPointFromHistory(current + nextPoint, start), out _)) return false;
-
-            string hash = MD5Hasher.GetHash($"{_secretHash}{current}");
-
-            // 0U 1D 2L 3R 
-            bool[] dirs = new bool[4];
-
-            for (int i = 0; i < 4; i++)
-            {
-                dirs[i] = "bcdef".Contains(hash[i]);
-            }
-
-            if (nextPoint == "R" && !dirs[RIGHT]) return false;
-            if (nextPoint == "L" && !dirs[LEFT]) return false;
-            if (nextPoint == "U" && !dirs[UP]) return false;
-            if (nextPoint == "D" && !dirs[DOWN]) return false;
-
-            return true;
-        }
-    
-        public void DisplayMap() => DisplayMap(FinalPath);
-  
-        public void DisplayMap(List<Point3D> drawPath)
-        {
-            Point3D cursor = new();
-
-            for (int y = MapMax.Y; y >= MapMin.Y; y--)
-            {
-                for (int x = MapMin.X; x <= MapMax.X; x++)
-                {
-                    cursor.X = x;
-                    cursor.Y = y;
-
-                    //if (_theMap.TryGetValue(cursor.Get2DPoint(), out MapSymbols value))
-                    //{
-                    //    if (drawPath.Contains(cursor)) Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                    //    if (cursor.Get2DPoint() == _defaultStart) Console.ForegroundColor = ConsoleColor.Green;
-                    //    if (cursor.Get2DPoint() == _defaultEnd) Console.ForegroundColor = ConsoleColor.Blue;
-
-                    //    Console.Write((char)(value + 96));
-
-                    //    if (Console.ForegroundColor != ConsoleColor.Gray) Console.ForegroundColor = ConsoleColor.Gray;
-                    //}
-                    //else
-                    //{
-                    //    Console.ForegroundColor = ConsoleColor.Red;
-                    //    Console.Write("?");
-                    //    Console.ForegroundColor = ConsoleColor.Gray;
-                    //}
-                }
-                Console.WriteLine(" ");
-            }
-        }
+    private enum Direction
+    {
+        UP = 0,
+        DOWN = 1,
+        LEFT = 2,
+        RIGHT = 3
     }
 }
--- 2016/Day 17/Program.cs
+++ 2016/Day 17/Program.cs
@@ -5,13 +5,12 @@ try
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string puzzleInput = File.ReadAllText(PUZZLE_INPUT);
 
-
     Map theMap = new(puzzleInput);
-    theMap.A_Star(theMap.DefaultStart, theMap.DefaultEnd, out _);
+    theMap.A_Star();
 
     Console.WriteLine($"Part 1: The final path is {theMap.History}.");
 
-    int part2Answer = theMap.A_Slow(theMap.DefaultStart, theMap.DefaultEnd);
+    int part2Answer = theMap.A_Slow();
     Console.WriteLine($"Part 2: The longest path is {part2Answer}.");
 }
 catch (Exception e)
