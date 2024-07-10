75ea4c09dbf2b979b7613f8e88b1552eabed034d
Kezzryn <bkholmes@shaw.ca>
2024-07-10 12:54:22 -0500

    2017 Day 24, 25 .net8, and cleanup.

--- 2017/Day 24/Program.cs
+++ 2017/Day 24/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System.Numerics;
 
-void calculateBridgeStrength(Dictionary<ulong, (int plugA, int plugB)> plugs, out int strongestBridge, out int longestBridgeStrength )
+static void CalculateBridgeStrength(Dictionary<ulong, (int plugA, int plugB)> plugs, out int strongestBridge, out int longestBridgeStrength)
 {
-
     // bridge contents, and open plug
     PriorityQueue<(ulong, int), int> queue = new();
-    HashSet<ulong> queueContents = new();
+    HashSet<ulong> queueContents = [];
 
     queue.Enqueue((0, 0), 0);
     queueContents.Add(0);
@@ -14,9 +13,8 @@ void calculateBridgeStrength(Dictionary<ulong, (int plugA, int plugB)> plugs, ou
     longestBridgeStrength = 0;
     strongestBridge = 0;
 
-    while (queue.Count > 0)
+    while (queue.TryDequeue(out (ulong bridge, int openPlug) q, out int bridgeStrength))
     {
-        queue.TryDequeue(out (ulong bridge, int openPlug) q, out int bridgeStrength);
         queueContents.Remove(q.bridge);
 
         if (bridgeStrength > strongestBridge)
@@ -31,11 +29,10 @@ void calculateBridgeStrength(Dictionary<ulong, (int plugA, int plugB)> plugs, ou
             longestBridgeStrength = bridgeStrength;
         }
 
-
         // search the dictionary for values that contain the current open plug
         foreach (KeyValuePair<ulong, (int, int)> currentPlug in plugs.Where(x => x.Value.plugA == q.openPlug || x.Value.plugB == q.openPlug))
         {
-            if ((q.bridge & currentPlug.Key) != 0) continue;
+            if ((q.bridge & currentPlug.Key) != 0) continue; //skip used plugs
 
             ulong newBridgeState = q.bridge | currentPlug.Key;
 
@@ -50,13 +47,12 @@ void calculateBridgeStrength(Dictionary<ulong, (int plugA, int plugB)> plugs, ou
     }
 }
 
-
 try
 {
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string[] puzzleInput = File.ReadAllLines(PUZZLE_INPUT);
 
-    Dictionary<ulong, (int plugA, int plugB)> plugs = new();
+    Dictionary<ulong, (int plugA, int plugB)> plugs = [];
 
     ulong plugID = 1;
     foreach (string line in puzzleInput)
@@ -66,7 +62,7 @@ try
         plugID <<= 1;
     }
 
-    calculateBridgeStrength(plugs, out int part1Answer, out int part2Answer);
+    CalculateBridgeStrength(plugs, out int part1Answer, out int part2Answer);
 
     Console.WriteLine($"Part 1: The strongest bridge we can make is {part1Answer}.");
     Console.WriteLine($"Part 2: The strength of the longest bridge we can make is {part2Answer}.");
--- 2017/Day 25/Program.cs
+++ 2017/Day 25/Program.cs
@@ -6,49 +6,41 @@
 
     string[][] puzzleInput = File.ReadAllText(PUZZLE_INPUT).Split(CRLF+CRLF).Select(x => x.Split(CRLF).ToArray()).ToArray();
 
-    int currentState = 0;
-    int numLoops = 0;
     int cursor = 0;
 
-    HashSet<int> tape = new();
-    Dictionary<int, (int nextValue, int nextState, int nextPOS)> nextStep = new();
+    HashSet<int> tape = [];
+    Dictionary<int, (int nextValue, int nextState, int nextPOS)> nextStep = [];
 
-    for (int i = 0; i < puzzleInput.Length; i++)
+    //Begin in state X.
+    //Perform a diagnostic checksum after 1234 steps.
+    int currentState = puzzleInput[0][0].ToCharArray().TakeLast(2).First();
+    int numLoops =     int.Parse(puzzleInput[0][1].Split(' ')[5]);
+
+    for (int i = 1; i < puzzleInput.Length; i++)
     {
-        if (i == 0)
-        {
-            //Begin in state X.
-            //Perform a diagnostic checksum after 1234 steps.
+        //In state X:                               line 0
+        //  If the current value is 0:              line 1
+        //    - Write the value 1.                  line 2
+        //    - Move one slot to the [right/left].  line 3
+        //    - Continue with state Y.              line 4
 
-            currentState = puzzleInput[i][0].ToCharArray().TakeLast(2).First();
-            numLoops =     int.Parse(puzzleInput[i][1].Split(' ')[5]);
-        }
-        else
-        {
-            //In state X:                               line 0
-            //  If the current value is 0:              line 1
-            //    - Write the value 1.                  line 2
-            //    - Move one slot to the [right/left].  line 3
-            //    - Continue with state Y.              line 4
+        int thisState = puzzleInput[i][0].ToCharArray().TakeLast(2).First();
+        int value =     puzzleInput[i][2].ToCharArray().TakeLast(2).First() == '1' ? 1 : 0;
+        int POS =       puzzleInput[i][3].Contains("right") ? 1 : -1;
+        int state =     puzzleInput[i][4].ToCharArray().TakeLast(2).First(); 
 
-            int thisState = puzzleInput[i][0].ToCharArray().TakeLast(2).First();
-            int value =     puzzleInput[i][2].ToCharArray().TakeLast(2).First() == '1' ? 1 : 0;
-            int POS =       puzzleInput[i][3].Contains("right") ? 1 : -1;
-            int state =     puzzleInput[i][4].ToCharArray().TakeLast(2).First(); 
+        nextStep.Add(thisState, (value, POS, state));
 
-            nextStep.Add(thisState, (value, POS, state));
-
-            //  If the current value is 1:              line 5
-            //    - Write the value 1.                  line 6
-            //    - Move one slot to the [right/left].  line 7
-            //    - Continue with state Y.              line 8
+        //  If the current value is 1:              line 5
+        //    - Write the value 1.                  line 6
+        //    - Move one slot to the [right/left].  line 7
+        //    - Continue with state Y.              line 8
             
-            value =         puzzleInput[i][6].ToCharArray().TakeLast(2).First() == '1' ? 1 : 0;
-            POS =           puzzleInput[i][7].Contains("right") ? 1 : -1;
-            state =         puzzleInput[i][8].ToCharArray().TakeLast(2).First();
+        value =         puzzleInput[i][6].ToCharArray().TakeLast(2).First() == '1' ? 1 : 0;
+        POS =           puzzleInput[i][7].Contains("right") ? 1 : -1;
+        state =         puzzleInput[i][8].ToCharArray().TakeLast(2).First();
                 
-            nextStep.Add(thisState | STATE_TRUE, (value, POS, state));
-        }
+        nextStep.Add(thisState | STATE_TRUE, (value, POS, state));
     }
 
     for (int i = 1; i <= numLoops; i++)
