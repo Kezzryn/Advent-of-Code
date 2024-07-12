f8f9891dd90f632a99ce6a07c6cff237392fbd53
Kezzryn <bkholmes@shaw.ca>
2024-07-11 21:13:55 -0500

    2017 Day 15 .net 8 and reworked.

---
+++ 2017/Day 15/Generator.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC_2017_Day_15;
+
+internal class Generator(int factor, long startValue)
+{
+    const int MODULO = 2147483647;
+
+    private readonly int _factor = factor;
+    private int _divisor = 1;
+    private long _value = startValue;
+    private readonly long _startvalue = startValue;
+
+    public void ResetForPart2(int newDivisor)
+    {
+        _value = _startvalue;
+        _divisor = newDivisor;
+    }
+
+    public ushort NextNum()
+    {
+        do
+        {
+            _value = (_value * _factor) % MODULO;
+        } while (_value % _divisor != 0);
+
+        return (ushort)_value;
+    }
+}
--- 2017/Day 15/Program.cs
+++ 2017/Day 15/Program.cs
@@ -1,61 +1,28 @@
-﻿try
+﻿using AoC_2017_Day_15;
+
+const int GEN_A_FACTOR = 16807;
+const int GEN_B_FACTOR = 48271;
+
+const int PART_1_NUM_PAIRS = 40_000_000;
+const int PART_2_NUM_PAIRS = 5_000_000;
+
+const int PART_2_FACTOR_A = 4;
+const int PART_2_FACTOR_B = 8;
+
+try
 {
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string[] puzzleInput = File.ReadAllLines(PUZZLE_INPUT);
 
-    const bool DO_PART_2 = true;
+    Generator GenA = new(GEN_A_FACTOR, int.Parse(puzzleInput[0][^3..]));
+    Generator GenB = new(GEN_B_FACTOR, int.Parse(puzzleInput[1][^3..]));
 
-    const long PART_1_NUM_PAIRS = 40000000;
-    const long PART_2_NUM_PAIRS = 5000000;
+    int part1Answer = Enumerable.Range(0, PART_1_NUM_PAIRS).Count(x => GenA.NextNum() == GenB.NextNum());
 
-    long startValueGenA = int.Parse(puzzleInput[0][^3..]);
-    long startValueGenB = int.Parse(puzzleInput[1][^3..]); 
-    
-    static long GeneratorSolve(long valueGenA, long valueGenB, long numPairs, bool doPart2 = false)
-    {
-        const long GEN_A_FACTOR = 16807;
-        const long GEN_B_FACTOR = 48271;
-        const long MODULO = 2147483647;
+    GenA.ResetForPart2(PART_2_FACTOR_A);
+    GenB.ResetForPart2(PART_2_FACTOR_B);
 
-        Queue<ushort> queueGenA = new();
-        Queue<ushort> queueGenB = new();
-
-        long count = 0;
-        long rv = 0;
-
-        long divisorGenA = doPart2 ? 4 : 1;
-        long divisorGenB = doPart2 ? 8 : 1;
-
-        while (count < numPairs)
-        {
-            valueGenA = (valueGenA * GEN_A_FACTOR) % MODULO;
-            if (valueGenA % divisorGenA == 0 )
-            {
-                queueGenA.Enqueue((ushort)valueGenA);
-            }
-            
-            valueGenB = (valueGenB * GEN_B_FACTOR) % MODULO;
-            if (valueGenB % divisorGenB == 0)
-            {
-                queueGenB.Enqueue((ushort)valueGenB);
-            }
-
-            if (queueGenA.Count > 0 &&  queueGenB.Count > 0)
-            {
-                count++;
-                ushort GenA = queueGenA.Dequeue();
-                ushort GenB = queueGenB.Dequeue();
-
-                if (GenA == GenB) rv++;
-            }
-        }
-
-        return rv;
-    }
-
-    long part1Answer = GeneratorSolve(startValueGenA, startValueGenB, PART_1_NUM_PAIRS);
-
-    long part2Answer = GeneratorSolve(startValueGenA, startValueGenB, PART_2_NUM_PAIRS, DO_PART_2);
+    int part2Answer = Enumerable.Range(0, PART_2_NUM_PAIRS).Count(x => GenA.NextNum() == GenB.NextNum());
 
     Console.WriteLine($"Part 1: There are {part1Answer} matching pairs.");
     Console.WriteLine($"Part 2: There are {part2Answer} matching pairs when they try to syncronize.");
