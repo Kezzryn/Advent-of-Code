ff80d43802693b6fe60cc5483bf240704daf3f7d
Kezzryn <bkholmes@shaw.ca>
2024-07-14 16:24:08 -0500

    2018 Day 03 .net8 converted to use Rectangle class.

--- 2018/Day 03/Program.cs
+++ 2018/Day 03/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using AoC_2018_Day_03;
+using BKH.Geometry;
 using System.Text.RegularExpressions;
 
 try
@@ -6,45 +7,34 @@ try
     const string PUZZLE_INPUT = "PuzzleInput.txt";
     string[] puzzleInput = File.ReadAllLines(PUZZLE_INPUT);
 
-    Dictionary<int, Rectangle> elfPlans = new();
-    int[,] cloth = new int[1000, 1000];
+    RectGroup overlappingRects = new();
+    Dictionary<int, Rectangle> elfPlans = [];
 
     foreach (string line in puzzleInput)
     {
-        int[] elfdata = Regex.Matches(line, @"\d+").Select(x => int.Parse(x.Value)).ToArray();
-        elfPlans.Add(elfdata[0], new Rectangle(elfdata[1], elfdata[2], elfdata[3], elfdata[4]));
+        int[] elfdata = GetNumbers().Matches(line).Select(x => int.Parse(x.Value)).ToArray();
+        elfPlans.Add(elfdata[0], new Rectangle(
+            elfdata[1], elfdata[2],
+            elfdata[1] + elfdata[3], elfdata[2] + elfdata[4]));
     }
 
-    int part1Answer = 0;
     int part2Answer = 0;
 
     foreach((int ID1, Rectangle planA) in  elfPlans)  
     {
         bool isIntersection = false;
-        foreach((int ID2, Rectangle planB) in elfPlans) 
+        foreach ((int ID2, Rectangle planB) in elfPlans.Where(x => x.Key != ID1))
         {
-            if (ID1 == ID2) continue;
-            if (planA.IntersectsWith(planB))
+            if (planA.Overlap(planB))
             {
                 isIntersection = true;
-                Rectangle intersection = Rectangle.Intersect(planA, planB);
-                foreach ((int x, int y) in from x in Enumerable.Range(intersection.X, intersection.Width)
-                                           from y in Enumerable.Range(intersection.Y, intersection.Height)
-                                           select (x, y))
-                {
-                    cloth[x, y]++;
-                }
+                overlappingRects.Add(planA.Intersect(planB));
             }
         }
         if (!isIntersection) part2Answer = ID1;
     }
-    
-    foreach((int x, int y) in from x in Enumerable.Range(0, cloth.GetUpperBound(0))
-                              from y in Enumerable.Range(0, cloth.GetUpperBound(1))
-                              select (x, y))
-    {
-        if (cloth[x,y] >= 2) part1Answer++;
-    }
+
+    long part1Answer = overlappingRects.Rectangles.Sum(x => x.Area);
 
     Console.WriteLine($"Part 1: There are {part1Answer} square inches of fabric that have multiple claims.");
     Console.WriteLine($"Part 2: {part2Answer} is the only ID that doesn't overlap.");
@@ -52,4 +42,10 @@ try
 catch (Exception e)
 {
     Console.WriteLine(e);
+}
+
+partial class Program
+{
+    [GeneratedRegex(@"\d+")]
+    private static partial Regex GetNumbers();
 }
---
+++ 2018/Day 03/RectGroup.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC_2018_Day_03;
+using BKH.Geometry;
+internal class RectGroup
+{
+    public readonly List<Rectangle> Rectangles = [];
+    private readonly Queue<Rectangle> queue = [];
+
+    public void Add(Rectangle newRect)
+    {
+        queue.Enqueue(newRect);
+
+        while (queue.TryDequeue(out Rectangle? queueRect))
+        {
+            int stopIndex = Rectangles.Count - 1;
+            for (int i = 0; i <= stopIndex; i++)
+            {
+                if (queueRect.Overlap(Rectangles[i]))
+                {
+                    Rectangles.AddRange(Rectangles[i].Exclude(queueRect));
+                    Rectangles.RemoveAt(i--);
+                    stopIndex--;
+                }
+            }
+
+            Rectangles.Add(queueRect);
+        }
+    }
+}
