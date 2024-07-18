fc8627aab44aa365eff53a91148dbfb7c16aad2e
Kezzryn <bkholmes@shaw.ca>
2024-07-17 19:08:44 -0500

    2018 Day 06 .net 8, Point2D, cleanup.

--- 2018/Day 06/Program.cs
+++ 2018/Day 06/Program.cs
@@ -1,37 +1,35 @@
-﻿using System.Drawing;
-
-static int TaxiDistance(Point a, Point b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+﻿using BKH.Geometry;
 
 try
 {
     const int DUPE_DEFAULT = -1;
     const int PART_2_REGION_SIZE = 10000;
     const string PUZZLE_INPUT = "PuzzleInput.txt";
-    Dictionary<Point, int> puzzleInput = File.ReadAllLines(PUZZLE_INPUT)
+    Dictionary<Point2D, int> puzzleInput = File.ReadAllLines(PUZZLE_INPUT)
             .Select(x => x.Split(", "))
-            .Select(x => new Point(int.Parse(x.First()), int.Parse(x.Last())))
+            .Select(x => new Point2D(int.Parse(x.First()), int.Parse(x.Last())))
             .ToDictionary(x => x, x => 0);
 
     // determine the corners.
     // Experimentation shows that this encapsulates the area needed for part two.
-    Point min = new(puzzleInput.Min(x => x.Key.X), puzzleInput.Min(x => x.Key.Y));
-    Point max = new(puzzleInput.Max(x => x.Key.X), puzzleInput.Max(x => x.Key.Y));
+    Point2D min = new(puzzleInput.Min(x => x.Key.X), puzzleInput.Min(x => x.Key.Y));
+    Point2D max = new(puzzleInput.Max(x => x.Key.X), puzzleInput.Max(x => x.Key.Y));
 
-    HashSet<Point> infinitePoints = new();
+    HashSet<Point2D> infinitePoints = [];
 
     int part2Answer = 0;
-    foreach (Point gridPoint in from x in Enumerable.Range(min.X, max.X - min.X)
-                                from y in Enumerable.Range(min.Y, max.Y - min.Y)
-                                select new Point(x, y))
+    foreach (Point2D gridPoint in from x in Enumerable.Range(min.X, max.X - min.X)
+                                  from y in Enumerable.Range(min.Y, max.Y - min.Y)
+                                  select new Point2D(x, y))
     {
         int closestDistance = int.MaxValue;
         int dupeDistance = DUPE_DEFAULT;
-        Point closestPoint = new();
+        Point2D closestPoint = new();
 
         int sumDistance = 0;
-        foreach (Point inputPoint in puzzleInput.Keys)
+        foreach (Point2D inputPoint in puzzleInput.Keys)
         {
-            int distance = TaxiDistance(inputPoint, gridPoint);
+            int distance = Point2D.TaxiDistance2D(inputPoint, gridPoint);
             sumDistance += distance;
 
             if (distance == closestDistance) dupeDistance = distance;
@@ -59,7 +57,7 @@ try
     int part1Answer = puzzleInput.Where(x => !infinitePoints.Contains(x.Key)).Select(v => v.Value).Max();
 
     Console.WriteLine($"Part 1: The largest area is {part1Answer}.");
-    Console.WriteLine($"Part 2: The region is {part2Answer}");
+    Console.WriteLine($"Part 2: The region is {part2Answer}.");
 }
 catch (Exception e)
 {
